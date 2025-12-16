ecac401e7fd4ac39247d3654033120e0628ee5f4
Mohit kumar <mohitsinghoi501@gmail.com>
2025-12-16 16:13:41 +0530

    initial commit

--- Patterns/Pattern_03.java
+++ Patterns/Pattern_03.java
@@ -6,16 +6,34 @@
 // 1 2 3 4 5
 
 
+// package Patterns;
+// public class Pattern_03 {
+//     public static void main(String[] args) {
+//         int n=5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+    
+// }
+
+
 package Patterns;
-public class Pattern_03 {
-    public static void main(String[] args) {
-        int n=5;
+public class Pattern_03{
+    public static void  Pattern(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j);
             }
             System.out.println();
         }
+
     }
-    
+    public static void main(String[] args) {
+        int n=5;
+        Pattern_03.Pattern(n);
+    }
 }
