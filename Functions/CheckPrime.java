58a4207060852de611f3db1707f3debf7f467f70
Mohit kumar <mohitsinghoi501@gmail.com>
2025-12-23 16:55:55 +0530

    Update for Optimized solution

--- Functions/CheckPrime.java
+++ Functions/CheckPrime.java
@@ -2,25 +2,37 @@ package Functions;
 import java.util.Scanner;
 
 public class CheckPrime {
-  
-    //Only for n>=2
-    public static boolean primeCheck(int n){
-        // boolean isprime=true;
+  ////Generalized Code
+    // //Only for n>=2
+    // public static boolean primeCheck(int n){
+    //     // boolean isprime=true;
 
-        //corrner case
-        if(n==2){
-            return true;
-        }
-        for(int i=2;i<n;i++){
+    //     //corrner case
+    //     if(n==2){
+    //         return true;
+    //     }
+    //     for(int i=2;i<n;i++){
+    //         if(n%i==0){
+    //         // isprime=false;
+    //         return false;
+    //         // break;
+    //         }
+    //     }
+    // //    return isprime;
+    //    return true;
+    // }
+
+
+    //Optimized Code
+    public static boolean primeCheck(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0){
-            // isprime=false;
-            return false;
-            // break;
+                return false;
             }
         }
-    //    return isprime;
-       return true;
+        return  true;
     }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
