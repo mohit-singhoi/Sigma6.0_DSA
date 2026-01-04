e88c84971e16e7adf53f02806856f5d671544ad9
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-04 19:40:45 +0530

    Update ZeroOne Rectangle

--- PatternPrinting/Level_02/Pattern_05.java
+++ PatternPrinting/Level_02/Pattern_05.java
@@ -8,20 +8,39 @@
 package PatternPrinting.Level_02;
 
 public class Pattern_05 {
+    ////1st way
+    
+    // public static void zeroOne(int n){
+    //     boolean temp=true;
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=i;j++){
+    //             if(temp){
+    //                 System.out.print("1 ");
+    //             }else{
+    //                 System.out.print("0 ");
+    //             }
+    //             temp = !temp;
+    //         }
+    //         System.out.println();
+    //     }
+
+    ////2nd way
     public static void zeroOne(int n){
-        boolean temp=true;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                if(temp){
-                    System.out.print("1 ");
-                }else{
-                    System.out.print("0 ");
+                if((i+j)%2==0) { //even
+                    System.out.print("1"+" ");
+    
                 }
-                temp = !temp;
+                else{
+                    System.out.print("0"+" ");
+                }
             }
             System.out.println();
         }
     }
+
+    
     public static void main(String[] args) {
         int n=5;
         zeroOne(n);
