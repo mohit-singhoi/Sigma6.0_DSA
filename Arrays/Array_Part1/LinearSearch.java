0694860587dd09c0b47764dc505662872662e986
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-10 00:26:51 +0530

    Update linear search

--- Arrays/Array_Part1/LinearSearch.java
+++ Arrays/Array_Part1/LinearSearch.java
@@ -1,19 +1,35 @@
+import java.util.*;
+
 public class LinearSearch {
 
-    public static void  linearsearch(int marks[], int key){
-        for(int i=0;i<marks.length;i++){
-            if(marks[i]==key){
-                System.out.println("Key is found on the index of :"+i);
-                return;
-            }
+    // public static void linearsearch(int marks[], int key){
+    // for(int i=0;i<marks.length;i++){
+    // if(marks[i]==key){
+    // System.out.println("Key is found on the index of :"+i);
+    // return;
+    // }
+    // }
+    // System.out.println("Key not found");
+    // }
+
+    public static int linearSearch(int num[], int key) {
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == key)
+                return i;
         }
-        System.out.println("Key not found");
+        return -1;
     }
 
     public static void main(String[] args) {
-        int marks[]={89,78,98,69,76};
-        int key=69;
-        linearsearch(marks, key);
+        int num[] = { 89, 78, 98, 69, 76 };
+        int key = 69;
+        // linearsearch(marks, key);
+
+        int index = linearSearch(num, key);
+        if (index == -1)
+            System.out.println("Key not found");
+        else
+            System.out.println("Element at index :" +index);
     }
-    
+
 }
