ca1ba74e2572a0b95a6b7e01e0c690206c78c0e2
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-20 22:10:06 +0530

    Updated

--- Sorting Algorithm/Basic Sorting Algorithm/SelectonSort.java
+++ Sorting Algorithm/Basic Sorting Algorithm/SelectonSort.java
@@ -2,26 +2,29 @@ public class SelectonSort {
     public static void selectonSort(int arr[]) {
         int n = arr.length;
 
-        for (int i = 0; i < n - 2; i++) {
-            int smallest = arr[i];
-            for (int j = i + 1; j < n - 1; j++) {
-                if (smallest > arr[j]) {
-                    int temp = smallest;
-                    smallest = arr[j];
-                    arr[j] = temp;
+        for (int i = 0; i < n - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+
                 }
             }
+            // swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
     }
 
-    public static void printArr(int arr[]){
-            for(int i=0;i<arr.length;i++){
-                System.out.print(arr[i]+" ");
-            }
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
     public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2};
+        int arr[] = { 5, 4, 1, 3, 2 };
         System.out.print("Sorted Array is : ");
         selectonSort(arr);
         printArr(arr);
