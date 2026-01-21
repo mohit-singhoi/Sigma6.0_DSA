2683b9a823c9776368bd7f9f6fc0932d1f5eee05
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-21 15:56:00 +0530

    Inbuilt Arrays using Collections

--- Sorting Algorithm/Basic Sorting Algorithm/InbuiltSort.java
+++ Sorting Algorithm/Basic Sorting Algorithm/InbuiltSort.java
@@ -4,15 +4,20 @@ import java.util.*;
 public class InbuiltSort {
     public static void inbuiltSort(int arr[]){
         int n= arr.length;
-        Arrays.sort(arr);
+        //1st way thats containing total arrays
+        // Arrays.sort(arr);
+
+        //2nd way : That containes only that you given b/w the range 
+        //it sort only 1st 3 arrays element
+        Arrays.sort(arr,0,3);
+
+
 
         //Print Sorted Arrays
         System.out.print("Sorted Array : ");
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
-
-        
     }
 
     public static void main(String[] args) {
---
+++ Sorting Algorithm/InbuiltCollection.java
@@ -0,0 +1,30 @@
+//Wap to sort an array using collection in reversed order
+
+import java.util.*;
+
+public class InbuiltCollection {
+    public static void inbuiltCollection(Integer arr[]){
+        int n= arr.length;
+
+        ////1st way sort for whole array
+        //Arrays.sort(arr,Collections.reverseOrder());
+
+
+        //2nd way to sort in a given range 
+        //Sort Arrays from 0 to 3 rd element
+        Arrays.sort(arr,0,4,Collections.reverseOrder());
+
+
+        //Print sorted arrays
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        Integer arr[] ={5,2,8,12,0,76};
+        System.out.print("Arrays in Sorted Order : ");
+        inbuiltCollection(arr);
+        
+    }
+}
