a80bfb9b8cbb7c4739726cfdd10f3dbe1644be3e
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-26 22:34:36 +0530

    Updated

--- Arrays/2D Arrays/SearchMatrix.java
+++ Arrays/2D Arrays/SearchMatrix.java
@@ -8,28 +8,43 @@ public class SearchMatrix {
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        int row = 0;
-        int col = cols - 1;
+        // // 1st way
+        // int row = 0;
+        // int col = cols - 1;
 
-        while (row < rows && col >= 0) {
-            if (matrix[row][col] == target) {
-                return true;
-            } else if (matrix[row][col] > target) {
-                col--;
-            } else {
-                row++;
-            }
+        // while (row < rows && col >= 0) {
+        //     if (matrix[row][col] == target) {
+        //         return true;
+        //     } else if (matrix[row][col] > target) {
+        //         col--;
+        //     } else {
+        //         row++;
+        //     }
+        // }
+
+        // return false;
+
+        // 2nd way
+        int row = rows - 1;
+        int col = 0;
+        while (col < cols && row >= 0) {
+        if (matrix[row][col] == target) {
+        return true;
+        } else if (matrix[row][col] > target) {
+        row--;
+        } else
+        col++;
         }
-
         return false;
     }
+
     public static void main(String[] args) {
         int[][] matrix = {
-            {1, 4, 7, 11, 15},
-            {2, 5, 8, 12, 19},
-            {3, 6, 9, 16, 22},
-            {10, 13, 14, 17, 24},
-            {18, 21, 23, 26, 30}
+                { 1, 4, 7, 11, 15 },
+                { 2, 5, 8, 12, 19 },
+                { 3, 6, 9, 16, 22 },
+                { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 }
         };
         int target = 5;
         System.out.println(" Element found : " + searchMatrix(matrix, target)); // Output: true
@@ -37,5 +52,5 @@ public class SearchMatrix {
         target = 20;
         System.out.println(" Element found : " + searchMatrix(matrix, target)); // Output: false
     }
-    
+
 }
