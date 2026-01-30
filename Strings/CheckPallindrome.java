067ba4eaed2e23dd1476e4ae101e67f5ab6de1fa
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-30 22:38:52 +0530

    initial commit

--- Strings/CheckPallindrome.java
+++ Strings/CheckPallindrome.java
@@ -1,22 +1,23 @@
 public class CheckPallindrome {
 
-    public static boolean isPallindrome(String str){
-        for(int i=0;i<str.length()/2;i++){
-            if(str.charAt(i) !=str.chatAt(n-1-i)){
-                //not a pallindrome
+    public static boolean isPallindrome(String str) {
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                // not a pallindrome
                 return false;
 
             }
-
 
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String str ="racecar";
-        System.out.println("String is Pallindrome ? : " +isPallindrome(str));
-        
+        String str = "racecar";
+        System.out.println("String is Pallindrome ? : " + isPallindrome(str));
+
     }
-    
+
 }
