20080428c700ec90b6eb93d4526dddeab722dd37
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-31 14:07:13 +0530

    initial commit

---
+++ Strings/LargestStr.java
@@ -0,0 +1,16 @@
+public class LargestStr {
+    
+    public static void main(String[] args) {
+        String names[] ={"Ankit Raj","Dipak","Mohit", "Amit","Abhishek","Raja","Ankit"};
+
+        String largest = names[0];
+        for(int i=0;i<names.length;i++){
+            if(largest.compareToIgnoreCase(names[i])<0){
+                largest = names[i];
+
+            }
+        }
+
+        System.out.println("Largest String is : " +largest);
+    }
+}
