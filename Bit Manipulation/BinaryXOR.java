6311897fac3529abc9950cca490e6b50a5dce279
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-01 22:27:21 +0530

    initial commit

---
+++ Bit Manipulation/BinaryXOR.java
@@ -0,0 +1,10 @@
+public class BinaryXOR {
+    public static void main(String[] args) {
+        int a = 5;  // Binary: 0101
+        int b = 6;  // Binary: 0110
+        int result = a ^ b; // Binary XOR operation
+
+        System.out.println("The result of " + a + " ^ " + b + " is: " + result);
+    }   
+    
+}
