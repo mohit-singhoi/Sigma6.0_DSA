a6dcb7cf30a50f59f2139252393c762ef9795cb4
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-01 22:20:06 +0530

    initial commit

---
+++ Bit Manipulation/BinaryAND.java
@@ -0,0 +1,10 @@
+public class BinaryAND {
+    public static void main(String[] args) {
+        int a = 5;  // Binary: 0101
+        int b = 6;  // Binary: 0110
+        int result = a & b; // Binary AND operation
+
+        System.out.println("The result of " + a + " & " + b + " is: " + result);
+    }
+    
+}
---
+++ Bit Manipulation/BinaryOR.java
@@ -0,0 +1,10 @@
+public class BinaryOR {
+    public static void main(String[] args) {
+        int a = 5;  // Binary: 0101
+        int b = 6;  // Binary: 0110
+        int result = a | b; // Binary OR operation
+
+        System.out.println("The result of " + a + " | " + b + " is: " + result);
+    }
+    
+}
