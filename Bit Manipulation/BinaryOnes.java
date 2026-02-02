dba7506a604baa10c114162e945b9d54d06bb467
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-02 23:31:04 +0530

    initial commit

---
+++ Bit Manipulation/BinaryOnes.java
@@ -0,0 +1,9 @@
+public class BinaryOnes {
+    public static void main(String[] args) {
+        int a = 5;  // Binary: 0101
+        int result = ~a; // Binary One's Complement operation
+
+        System.out.println("The one's complement of " + a + " is: " + result);
+    }
+    
+}
