fa8c8e4506366f05738ec9dda3efd118bb2cb9e8
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-02 23:52:11 +0530

    initial commit

---
+++ Bit Manipulation/BinaryLeftShift.java
@@ -0,0 +1,10 @@
+public class BinaryLeftShift {
+    public static void main(String[] args) {
+        int a = 5;  // Binary: 0101
+        int shiftBy = 2;
+        int result = a << shiftBy; // Binary Left Shift operation
+
+        System.out.println("The result of left shifting " + a + " by " + shiftBy + " positions is: " + result);
+    }
+    
+}
