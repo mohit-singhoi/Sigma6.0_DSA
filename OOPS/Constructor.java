bd1dfb9b853783520b653968b62fb11130c832e8
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-08 22:58:45 +0530

    initail commit

--- OOPS/Constructor.java
+++ OOPS/Constructor.java
@@ -1,7 +1,14 @@
 public class Constructor {
     public static void main(String[] args) {
-        Student s1 = new Student("Mohit");
-        System.out.println("Student Name : "+s1.name);
+        // Student s1 = new Student("Mohit");
+        // System.out.println("Student Name : "+s1.name);
+
+        Student s1 = new Student();
+        Student s2 = new Student("Mohit");
+        System.out.println("Student Name : "+s2.name);
+        Student s3 = new Student(3);
+        System.out.println("Student Roll : "+s3.roll);
+
 
     }
     
@@ -11,9 +18,20 @@ class Student{
     String name;
     int roll;
 
+
+    // Non parameterized Constructor
+    Student(){
+        System.out.println("Constructor is Called...");
+    }
+
+    //Parameterized  Constructor  
     Student(String name){
         this.name = name;
         
+    }
+
+    Student(int roll){
+        this.roll = roll;
     }
 
 }
---
+++ OOPS/CopyConstructor.java
@@ -0,0 +1,62 @@
+public class CopyConstructor {
+    public static void main(String[] args) {
+        Bank b1 = new Bank();
+        b1.UserName = "Mohit Kumar";
+        b1.age = 22;
+        b1.pass = 379;
+
+        b1.txns[0] = 100;
+        b1.txns[1] = 755;
+        b1.txns[2] = 15000;
+
+        Bank b2 = new Bank(b1); // Copy All Properties
+        b2.pass = 973;
+        b1.txns[2] = 50000;
+
+        System.out.println("Marks");
+        for(int i=0;i<3;i++){
+            System.out.println(b2.txns[i]);
+        }
+
+
+
+    }
+}
+
+class Bank {
+    String UserName;
+    int age;
+    int pass;
+    int txns[];
+
+    // //Shallow Copy Constructor
+    // Bank(Bank b1) {
+    //     txns = new int [3];
+    //     this.UserName = b1.UserName;
+    //     this.age = b1.age;
+    //     this.txns = b1.txns; // Here only referenced is copied
+
+    // }
+
+    // Deep copy constructor
+    Bank(Bank b1){
+        txns = new int[3];
+        this.UserName = b1.UserName;
+        this.age = b1.age;
+        for(int i=0;i<txns.length;i++){
+            this.txns[i] = b1.txns[i];
+        }
+    }
+
+    // Non-parameterized Constructor
+    Bank() {
+        txns = new int [3];
+        System.out.println("Constructor Created...");
+    }
+
+    // parameterized Constructor
+    Bank(String UserName) {
+        txns = new int [3];
+        this.UserName = UserName;
+    }
+}
