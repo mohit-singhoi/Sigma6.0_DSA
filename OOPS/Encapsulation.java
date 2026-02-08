50858c85cf349deca9efc96e0a8d313f30664fa3
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-08 20:49:01 +0530

    initial commit

---
+++ OOPS/Encapsulation.java
@@ -0,0 +1,23 @@
+public class Encapsulation {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.setSalary(55000.0f);
+        System.out.println("Employee Salary : "+e1.getSalary());
+    }
+    
+}
+
+class Employee {
+    public String name;
+    private float salary;
+
+    public void setSalary(float sal){
+        salary = sal;
+    }
+
+     float getSalary(){
+        return salary;
+    }
+
+    
+}
