af49fd85935cc9f8a55e8694cd01b20fe4b72428
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-09 19:37:13 +0530

    initial commit

---
+++ OOPS/Inheritance/Main3.java
@@ -0,0 +1,63 @@
+package Inheritance;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Cow c = new Cow("Black");
+        c.jugaalee();
+
+        Mammal m = new Mammal();
+        m.swim();
+
+        Dog d = new Dog("Brown");
+        d.barks();
+    }
+    
+}
+
+class Animal{
+    String name;
+    String color = "Black";
+
+    void eat(){
+        System.out.println("Eating...");
+    }
+
+    void breath(){
+        System.out.println("Breathing....");
+    }
+}
+
+class Mammal extends Animal{
+    Mammal(){
+       // this.color = color;
+        color = "white";
+        System.out.println("Mammal Color is  : "+this.color);
+    }
+  
+    void swim(){
+        System.out.println("Swim");
+    }
+}
+
+class Dog extends Animal{
+    Dog(String color){
+        this.color = color;
+      // color = "Dark Black";
+        System.out.println("Dog Color is : "+this.color);
+    }
+    void barks(){
+        System.out.println("Barks");
+    }
+}
+
+class Cow extends Animal{
+    Cow(String color){
+        this.color = color;
+      //  color = "Black & White";
+        System.out.println("Cow Color is Black & White");
+    }
+
+    void jugaalee(){
+        System.out.println("Cow is chew the cud");
+    }
+}
--- OOPS/Inheritance/main2.java
+++ OOPS/Inheritance/main2.java
@@ -1,6 +1,6 @@
 package Inheritance;
 
-public class main2 {
+public class Main2 {
     public static void main(String[] args) {
         Manager m1 = new Manager();
 
