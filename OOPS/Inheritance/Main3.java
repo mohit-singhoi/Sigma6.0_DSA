7d289a439b21a5d09cb5a6114de518addc64f258
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-09 19:43:11 +0530

    initial commit

--- OOPS/Inheritance/Main3.java
+++ OOPS/Inheritance/Main3.java
@@ -4,60 +4,67 @@ public class Main3 {
     public static void main(String[] args) {
         Cow c = new Cow("Black");
         c.jugaalee();
+        c.eat();
+        c.breath();
 
         Mammal m = new Mammal();
         m.swim();
+        m.eat();
+        m.breath();
 
         Dog d = new Dog("Brown");
         d.barks();
+        d.eat();
+        d.breath();
     }
-    
+
 }
 
-class Animal{
+class Animal {
     String name;
     String color = "Black";
 
-    void eat(){
+    void eat() {
         System.out.println("Eating...");
     }
 
-    void breath(){
+    void breath() {
         System.out.println("Breathing....");
     }
 }
 
-class Mammal extends Animal{
-    Mammal(){
-       // this.color = color;
+class Mammal extends Animal {
+    Mammal() {
+        // this.color = color;
         color = "white";
-        System.out.println("Mammal Color is  : "+this.color);
+        System.out.println("Mammal Color is  : " + this.color);
     }
-  
-    void swim(){
+
+    void swim() {
         System.out.println("Swim");
     }
 }
 
-class Dog extends Animal{
-    Dog(String color){
+class Dog extends Animal {
+    Dog(String color) {
         this.color = color;
-      // color = "Dark Black";
-        System.out.println("Dog Color is : "+this.color);
+        // color = "Dark Black";
+        System.out.println("Dog Color is : " + this.color);
     }
-    void barks(){
+
+    void barks() {
         System.out.println("Barks");
     }
 }
 
-class Cow extends Animal{
-    Cow(String color){
+class Cow extends Animal {
+    Cow(String color) {
         this.color = color;
-      //  color = "Black & White";
+        // color = "Black & White";
         System.out.println("Cow Color is Black & White");
     }
 
-    void jugaalee(){
+    void jugaalee() {
         System.out.println("Cow is chew the cud");
     }
 }
