cfc03cb1f817f90fdf7a0546648813f6329a3070
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-11 21:04:27 +0530

    initial commit

---
+++ OOPS/Interfaces/Main1.java
@@ -0,0 +1,23 @@
+package Interfaces;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.color = "White & Black";
+        System.out.println("Dog Color is : " +d.color);
+        d.sound();
+    }
+    
+}
+
+interface Animal{
+    void sound(); // by default it is an abstract method
+}
+
+class Dog implements Animal{
+    String color;
+
+    public void sound(){
+        System.out.println("Dog Barks..");
+    }
+}
