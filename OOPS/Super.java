77b44e19c1a618e227ea16398e80f9d1bc8a1616
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-11 23:12:25 +0530

    initial commit

---
+++ OOPS/Super.java
@@ -0,0 +1,23 @@
+public class Super {
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        System.out.println("Horse Color is : " +h.color);
+        
+    }
+    
+}
+
+class Animal{
+    String color;
+    Animal(){
+        System.out.println("Animal Constructor is called.");
+    }
+}
+
+class Horse extends Animal{
+    Horse(){
+        super.color = "Brown";
+       // super(); // Default already  super constructor is called here when obj is created.
+        System.out.println("Horse Constructor is called");
+    }
+}
