39950673a9e98534c8c2055d34f13426bc39184c
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-12 22:24:41 +0530

    initial commit

--- OOPS/PracticeSet/Qns_04.java
+++ OOPS/PracticeSet/Qns_04.java
@@ -1,7 +1,9 @@
 public class Qns_04 {
     public static void main(String[] args) {
         Vehicle obj1 = new Car();
-        obj1.print();
+        obj1.print(); // Here use function overriding when fun() is same then derived or child class function is execute.
+
+       // obj1.print1(); // Because this method is undefined for the Vehicle class.
 
         Vehicle obj2 = new Vehicle();
         obj2.print();
@@ -18,5 +20,10 @@ class Vehicle{
 class Car extends Vehicle{
     void print(){
         System.out.println("Derived class (car)");
+
+    }
+
+    void print1(){
+        System.out.println("Derived class Car.(Print1 fun()");
     }
 }
---
+++ OOPS/PracticeSet/Qns_05.java
@@ -0,0 +1,19 @@
+public class Qns_05 {
+    public static void main(String[] args) {
+        System.out.println("No of Books : "+Book.count);
+        Book b1 = new Book(150);
+        Book b2 = new Book(225);
+
+        System.out.println("Total Books : " + Book.count);
+    }
+}
+
+class Book{
+    int price;
+    static int count;
+
+    public Book(int price){
+        this.price = price;
+        count++;
+    }
+}
