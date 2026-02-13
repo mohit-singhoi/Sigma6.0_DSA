37080c4a97e65686f242a39c210a614c2a12f12b
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-14 00:25:58 +0530

    initial commit

---
+++ Recursion/Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+    
+    public static int fact(int n){
+        if(n ==1 ||  n==0) return 1;
+
+      //  int fact =1;
+        return n*fact(n-1);
+    }
+
+    //Main fun
+    public static void main(String[] args) {
+        int n =5;
+        int result = fact(n);
+        System.out.println("factorial of a Number : " +result);
+    }
+}
