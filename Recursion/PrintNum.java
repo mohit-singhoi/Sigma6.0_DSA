8d5a53e336068c4d3c368bce03e12fcbd27d0fc3
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-13 23:42:16 +0530

    initial commit

--- Recursion/PrintNum.java
+++ Recursion/PrintNum.java
@@ -1,18 +1,20 @@
 //print the reverse of num from 10 to 1 using recursion. 
 
 public class PrintNum {
-    public static void main(String[] args) {
-        int n = 10;
-        printNum(n);
-    }
 
     public static void printNum(int n) {
-        if (n == 1){
+        if (n == 1) {
             System.out.println(n);
             return;
         }
 
-        System.out.println(n+" ");
+        System.out.println(n + " ");
         printNum(n - 1);
+    }
+
+    // Main Function
+    public static void main(String[] args) {
+        int n = 10;
+        printNum(n);
     }
 }
