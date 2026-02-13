10e7c6d6d1e53e676661a7066bd572b2fba6be7d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-13 23:39:56 +0530

    initial commit

---
+++ Recursion/PrintNum.java
@@ -0,0 +1,16 @@
+public class PrintNum {
+    public static void main(String[] args) {
+        int n = 10;
+        printNum(n);
+    }
+
+    public static void printNum(int n) {
+        if (n == 1){
+            System.out.println(n);
+            return;
+        }
+
+        System.out.println(n+" ");
+        printNum(n - 1);
+    }
+}
