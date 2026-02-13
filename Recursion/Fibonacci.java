1b109a52682d1b4d7e2efb69dc1242475dccde43
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-14 01:25:26 +0530

    initial commit

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,21 @@
+// WAP to print the Fibonacci series up to n numbers
+
+public class Fibonacci {
+    
+    public static int printFibonacci(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return printFibonacci(n-1)+printFibonacci(n-2);
+    }
+
+    // Main Method
+    public static void main(String[] args){
+        int n = 10;
+        // int result = printFibonacci(n);
+        // System.out.println("Fibonacci  of " + n + " is : "+result);    
+        for(int i=0;i<=n;i++){
+            System.out.print(printFibonacci(i)+" ");
+        }
+    }
+}
