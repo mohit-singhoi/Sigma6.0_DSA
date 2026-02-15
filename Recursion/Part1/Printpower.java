8b37ded733ce23bf7ca38f89f0178c1e729edaad
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-16 00:21:36 +0530

    initial commit

---
+++ Recursion/Part1/Printpower.java
@@ -0,0 +1,24 @@
+// WAP to print X^n using recursion
+// Example: 2^10 = 1024
+
+public class Printpower {
+
+    public static int printPowerOfN(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int xnm1 = printPowerOfN(x, n - 1);
+        int xn = x * xnm1;
+        return xn;
+    }
+
+    public static void main(String[] args) {
+
+        int x = 2;
+        int n = 10;
+
+        int result = printPowerOfN(x, n);
+        System.out.println("Power of X^n : " + result);
+    }
+}
