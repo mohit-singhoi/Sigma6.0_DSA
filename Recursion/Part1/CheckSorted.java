2e48b5640aeb181fec809386968d945b838ca315
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-15 23:30:32 +0530

    initial commit

---
+++ Recursion/Part1/CheckSorted.java
@@ -0,0 +1,22 @@
+// Check if a Given array is sorted or not.
+public class CheckSorted{
+
+    public static boolean isSorted(int arr[] , int i){
+        if(i == arr.length-1){
+            return true;
+        }
+
+        if(arr[i] >  arr[i+1]){
+            return false;
+        }
+
+        return isSorted(arr, i+1);
+    }
+
+    // Main Function
+    public static void main(String[] args){
+        int arr[] = {7,9,1,2,7,3};
+        System.out.println("Array is Sorted : " +isSorted(arr, 0));
+        
+    }
+}
