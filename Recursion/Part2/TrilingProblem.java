cbb8d1dc376bcf18b6c2543e7ccf65a5a13c7126
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-17 20:40:27 +0530

    initial commit

---
+++ Recursion/Part2/TrilingProblem.java
@@ -0,0 +1,27 @@
+// Given a " 2 x n" floor and tiles of size "2 X 1", count the number of ways to tile the given board using the 2x1  tiles.
+// ( A tile can  either be placed horizontally or verticle).
+package Part2;
+
+public class TrilingProblem {
+    public static int tilingProblem(int n){
+        // base case
+        if(n == 0 || n ==1 ){
+            return 1;
+        }
+
+        // Verticle choice
+        int fnm1 = tilingProblem(n-1);
+
+        // Horizontal choice
+        int fnm2 = tilingProblem(n-2);
+
+        int toWays = fnm1 + fnm2;
+        return toWays;
+    }
+
+    // Main fun
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println("Total No. of ways : " +tilingProblem(n));
+    }
+}
