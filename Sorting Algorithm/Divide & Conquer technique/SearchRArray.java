09f89d3e2fc86688c22f53d9bf40f10df3886d7d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-21 19:30:35 +0530

    initial commit

--- Sorting Algorithm/Divide & Conquer technique/SearchRArray.java
+++ Sorting Algorithm/Divide & Conquer technique/SearchRArray.java
@@ -1,7 +1,7 @@
 // WAP to search an element in a sorted and rotated array.
 // input : sorted , rotated array with distinct numbers(in ascending order) It is rotated at a pivot point. Find the index of given element.
-// Example : arr[] = {4, 5, 6, 7, 0, 1, 2} , target = 0
-// output : 4 (index of target element in the array)
+// Example : arr[] = {4, 5, 6, 7, 0, 1, 2} , target = 6
+// output : 6 (index of target element in the array)
 
 public class SearchRArray {
     public static int search(int arr[], int tar, int si, int ei){
@@ -46,7 +46,7 @@ public class SearchRArray {
 
     public static void main(String[] args) {
         int arr[] = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
+        int target = 6;
         int n = arr.length;
 
         int result = search(arr, target, 0, n - 1);
