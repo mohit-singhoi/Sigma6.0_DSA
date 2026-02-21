32ab8cd572760456231cf501cba73c396441680d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-02-21 18:08:33 +0530

    initial commit

---
+++ Sorting Algorithm/Divide & Conquer technique/MergeSort.java
@@ -0,0 +1,20 @@
+// Merge Sort is a divide-and-conquer algorithm that sorts an array by recursively dividing it into smaller subarrays, sorting those subarrays, and then merging them back together in the correct order.
+public class MergeSort {
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Original array:");
+        printArray(arr);
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
