dbf4fb442547cfecf9f81340714a7aacc95b3a6d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-10 21:32:47 +0530

    initial commit

--- Sorting Algorithm/Divide & Conquer technique/Practice Qns/Qns_07.java
+++ Sorting Algorithm/Divide & Conquer technique/Practice Qns/Qns_07.java
@@ -1,8 +1,54 @@
+// Quick Sort implementation in Java
+public class Qns_07 {
 
-public class Qns_07{
-  public static void main (String [] args){
-  System.out.println (" Qns 7");
-}
-}
- 
+    // Function to partition the array
+    static int partition(int arr[], int low, int high) {
 
+        int pivot = arr[high];   // choose last element as pivot
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] < pivot) {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // place pivot in correct position
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    // Quick Sort function
+    static void quickSort(int arr[], int low, int high) {
+
+        if (low < high) {
+
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);   // left
+            quickSort(arr, pi + 1, high);  // right
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {10, 7, 8, 9, 1, 5};
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
