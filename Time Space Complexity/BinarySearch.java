ca5ae7a6c8bd0e80d097acd92a8b4f7eea296b6a
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-12 21:58:19 +0530

    initial commit

---
+++ Time Space Complexity/BinarySearch.java
@@ -0,0 +1,32 @@
+// General Binary Search Algorithm
+public class BinarySearch {
+    public static int binarySearch(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2; // To avoid overflow
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                start = mid + 1; // Search in the right half
+            } else {
+                end = mid - 1; // Search in the left half
+            }
+        }
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) { // TC O(log n) and SC O(1)
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        int result = binarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+    
+}
