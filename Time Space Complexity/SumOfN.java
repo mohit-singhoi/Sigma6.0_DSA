f89abff860344851e693dc94fd1393f9b485adf1
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-12 23:06:01 +0530

    initial commit

---
+++ Time Space Complexity/SumOfN.java
@@ -0,0 +1,16 @@
+// WAP to Calculate the sum of n number using recursion
+public class SumOfN {
+    public static int Sum(int n){
+        if(n== 0){
+            return 0;
+        }
+        return n+Sum(n-1);
+    }
+
+    // Main
+    public static void main(String[] args) {
+        int n= 5;
+        System.out.println("Sum of N Numbers : " +Sum(n));
+    }
+    
+}
