5b0ad6b74bc3f4fdad788a150c6cc713eef4d146
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-14 17:11:58 +0530

    onitial commit

--- Time Space Complexity/PowerFun2.java
+++ Time Space Complexity/PowerFun2.java
@@ -4,20 +4,20 @@ public class PowerFun2 {
         if(n==0){
             return 1;
         }
-        int halfPower = power(x, n/2);
+        int halfPowersq = power(x, n/2) * power(x, n/2); // recursive call to calculate half power
         if(n%2!=0){ // if n is odd
-            return x*halfPower*halfPower;
+            return x*halfPowersq;
         }else{
-            return halfPower*halfPower;
+            return halfPowersq;
         }
     }
 
     // Main function
-    public static void main(String[] args) {
+    public static void main(String[] args) { // TC = O(n) and SC = O(n) due to recursive calls
         int x = 2;
         int n = 5;
         System.out.println(x + " raised to the power " + n + " is : " + power(x, n));
-        // Time Complexity : O(log N) , Space Complexity : O(log N)
+        
     }
     
 }
