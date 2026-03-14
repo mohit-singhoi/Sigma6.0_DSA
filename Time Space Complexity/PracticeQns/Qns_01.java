1787368a0b69ab64533a1e25ee5fe8abd48cb987
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-14 17:39:56 +0530

    initial commit

---
+++ Time Space Complexity/PracticeQns/Qns_01.java
@@ -0,0 +1,24 @@
+package PracticeQns;
+
+public class Qns_01 {
+    public static int linearSearch(int[] arr, int target){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 3;
+        int result = linearSearch(arr, target);
+        if(result != -1){
+            System.out.println("Element found at index: " + result);
+        }else{
+            System.out.println("Element not found in the array.");  
+        }
+    }
+}
