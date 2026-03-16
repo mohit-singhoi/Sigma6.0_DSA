71d0f29f2918fc0e40b2473ecc181a1d3e11788b
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-16 23:46:17 +0530

    initial commit

--- Backtracking/NQueenProblem.java
+++ Backtracking/NQueenProblem.java
@@ -1,5 +1,30 @@
 // NQueens Problem: Place N queens on an N x N chessboard such that no two queens threaten each other.
 public class NQueenProblem {
+
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
     public static void nQueens(char board[][], int row) {
         // base
         if (row == board.length) {
@@ -9,9 +34,11 @@ public class NQueenProblem {
 
         // Column loop
         for (int j = 0; j < board.length; j++) {
-            board[row][j] = 'Q';
-            nQueens(board, row + 1); // Function call
-            board[row][j] = 'x'; // backtracking step
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1); // Function call
+                board[row][j] = 'x'; // backtracking step
+            }
         }
     }
 
@@ -27,7 +54,7 @@ public class NQueenProblem {
 
     // main fun
     public static void main(String[] args) {
-        int n= 2;
+        int n= 4;
         char board[][] = new char[n][n];
         // initialize queens
         for(int i=0;i<n;i++){
