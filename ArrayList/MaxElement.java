eadfd8c380a4e96d487a585e63a585d195bee5e2
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-20 23:47:31 +0530

    initial commit

--- ArrayList/MaxElement.java
+++ ArrayList/MaxElement.java
@@ -9,11 +9,16 @@ public class MaxElement {
         list.add(14);
         list.add(5);
 
-        int max = list.get(0);
+       // int max = list.get(0);
+        int max = Integer.MIN_VALUE; // it will work even if the list contains negative numbers.
         for (int i = 1; i < list.size(); i++) {
-            if (list.get(i) > max) {
-                max = list.get(i);
-            }
+            // 1. Using if condition
+            // if (list.get(i) > max) {
+            //     max = list.get(i);
+            // }
+
+            // 2. Using Math.max() method
+            max = Math.max(max, list.get(i));
         }
         System.out.println("Maximum element in the ArrayList is: " + max);
     }
