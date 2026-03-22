3a2901c147511115608dacfdee2ac9d33db67a50
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-22 23:07:34 +0530

    initial commit

---
+++ ArrayList/WaterContainer1.java
@@ -0,0 +1,43 @@
+
+//  Container with most water problem
+import java.util.ArrayList;
+
+public class WaterContainer1 {
+    public static int storeWater(ArrayList<Integer> height){
+        int maxWater =0;
+
+        // two pointer approach  TC: O(n)
+        int lp =0;
+        int rp = height.size()-1;
+
+        while(lp<rp){
+            int h = Math.min(height.get(lp), height.get(rp));
+            int w = rp-lp;
+            int currWater = h*w;
+            maxWater = Math.max(maxWater, currWater);
+
+            if(height.get(lp)<height.get(rp)){
+                lp++;
+            }else{
+                rp--;
+            }
+        }
+        return maxWater;
+    }
+    public static void main(String[] args) { 
+        ArrayList<Integer> height = new ArrayList<>();
+        // Height : 1,8,6,2,5,4,8,3,7
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+
+        System.out.println("Maximum water stored: " + storeWater(height));
+    }
+}
