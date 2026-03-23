5f30328ec02ccd7526ada00c46e4cfa60b27b4ea
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-23 23:57:13 +0530

    initial commit

--- ArrayList/PracticeQns/Qns_03.java
+++ ArrayList/PracticeQns/Qns_03.java
@@ -5,35 +5,35 @@ import java.util.*;
 public class Qns_03 {
 
     public static int mostFrequent(ArrayList<Integer> nums, int key) {
-        int maxCount = 0;
-        int result = -1;
 
+        int[] freq = new int[1000]; // assuming values are <= 1000
+
+        // count frequency of targets after key
         for (int i = 0; i < nums.size() - 1; i++) {
-
             if (nums.get(i) == key) {
                 int target = nums.get(i + 1);
-                int count = 0;
-
-                // count how many times this target follows key
-                for (int j = 0; j < nums.size() - 1; j++) {
-                    if (nums.get(j) == key && nums.get(j + 1) == target) {
-                        count++;
-                    }
-                }
-
-                if (count > maxCount) {
-                    maxCount = count;
-                    result = target;
-                }
+                freq[target]++;   // no need for -1 adjustment
             }
         }
 
-        return result;
+        int max = Integer.MIN_VALUE;
+        int ans = 0;
+
+        // find maximum frequency
+        for (int i = 0; i < 1000; i++) {
+            if (freq[i] > max) {
+                max = freq[i];
+                ans = i;
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
 
         ArrayList<Integer> nums = new ArrayList<>();
+
         nums.add(1);
         nums.add(100);
         nums.add(200);
