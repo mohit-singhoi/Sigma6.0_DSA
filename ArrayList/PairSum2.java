0a455dd92c42468a5745abe06499e596cee1e068
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-23 22:12:00 +0530

    initial commit

--- ArrayList/PairSum2.java
+++ ArrayList/PairSum2.java
@@ -7,21 +7,29 @@ public class PairSum2 {
 
     // Two Pointer Approach : O(n)
     public static boolean pairSum(ArrayList<Integer> list, int target) {
-        int lp = 0;
-        int rp = list.size() - 1;
-        while (lp < rp) {
+        int bp = -1;
+        int n = list.size();
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)> list.get(i+1)){ // breaking point
+                bp = i;
+                break;
+            }
+        }
+
+        int lp = bp+1; // smallest element
+        int rp = bp; // large element
+        while (lp != rp) {
             // Case 1
             if (list.get(lp) + list.get(rp) == target)
                 return true;
 
             // Case 2
-            if (list.get(lp) + list.get(rp) < target)
-                lp++;
-
-            // Case 3
-            if (list.get(lp) + list.get(rp) > target)
-                rp--;
-
+            if (list.get(lp) + list.get(rp) < target){
+                lp = (lp+1)% n;
+            }else{
+                // Case 3
+                rp = (n+rp-1) %n;
+            }
         }
         return false;
 
@@ -30,12 +38,14 @@ public class PairSum2 {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        // 11, 15, 6, 8, 9, 10 - Sorted & Rotated ArrayList
+        // Adding elements in the list
+        list.add(11);
+        list.add(15);
         list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
 
         int target;
 
