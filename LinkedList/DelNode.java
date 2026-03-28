546ef74b0a78e0638240e8cbd4abc8d3f3da4244
Mohit kumar <mohitsinghoi501@gmail.com>
2026-03-28 22:02:45 +0530

    initial commmit

---
+++ LinkedList/DelNode.java
@@ -0,0 +1,95 @@
+// Find and remove nth Node from End using iterative approach.
+public class DelNode {
+      public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+    // Methods
+    // Add element at 1st node - O(1)
+    public void addFirst(int data){
+
+        // Step 1 =  create new Node
+         Node newNode = new Node(data);
+         size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        // Step 2 newNode next = head
+        newNode.next = head; // Link
+
+        // Step 3 - head = newNode
+        head = newNode;
+    }
+
+
+    // print the linked list  - O(n)
+    public void printData(){
+        if(head == null){
+            System.out.println("LinkedList is Empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+
+            System.out.print(temp.data+ "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void deleteNthFromEnd(int n){
+        // Calculate LL Size
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+        if(n == sz){
+            head = head.next; // remove first
+            return;
+        }
+
+        //sz-n
+        int i =1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i<iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+
+    }
+
+    // Main fun
+    public static void main(String[] args) {
+        DelNode n1 = new DelNode();
+        n1.addFirst(8);
+        n1.addFirst(9);
+        n1.addFirst(4);
+        n1.addFirst(1);
+        n1.addFirst(0);
+        n1.addFirst(7);
+        n1.printData();
+
+        // Remove element 9
+        n1.deleteNthFromEnd(3);
+        n1.printData();
+    }
+
+}
