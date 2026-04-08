64de43caca49745679dff172d894a5d57735bfd2
Mohit kumar <mohitsinghoi501@gmail.com>
2026-04-08 23:34:41 +0530

    initial commit

---
+++ Stacks/Part1/stackB.java
@@ -0,0 +1,70 @@
+package Part1;
+
+import java.util.ArrayList;
+
+public class stackB {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static class Stack {
+        static Node head = null;
+
+        // isEmpty
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        // push
+        public static void push(int data) {
+            Node newNode = new Node(data);
+
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head; // link new node to old head
+            head = newNode; // update head
+        }
+
+        // pop
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+
+        while (!Stack.isEmpty()) {
+            System.out.println(Stack.peek());
+            Stack.pop();
+        }
+
+    }
+
+}
