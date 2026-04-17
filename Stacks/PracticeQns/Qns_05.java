0272a70460546f4e982df3edb3f8430185a03b5d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-04-18 00:22:24 +0530

    initial commit

--- Stacks/PracticeQns/Qns_05.java
+++ Stacks/PracticeQns/Qns_05.java
@@ -6,8 +6,9 @@
 // Explanation: Next greater element for 1 is 3, for 3 is 4
 
 import java.util.Stack;
+
 public class Qns_05 {
-    public static int [] nextGreaterElement(int[] arr) {
+    public static int[] nextGreaterElement(int[] arr) {
         int n = arr.length;
         int[] result = new int[n];
         Stack<Integer> stack = new Stack<>();
@@ -16,19 +17,25 @@ public class Qns_05 {
             while (!stack.isEmpty() && stack.peek() <= arr[i]) {
                 stack.pop();
             }
-            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            if (stack.isEmpty())
+                result[i] = -1;
+
+            else
+                result[i] = stack.peek();
+
             stack.push(arr[i]);
         }
 
         return result;
-    }   
 
-    // 
+    }
+
+    //
 
     // Driver code
     public static void main(String[] args) {
-        int[] arr1 = {1, 3, 2, 4};
-        int[] arr2 = {4, 5, 2, 10, 8};
+        int[] arr1 = { 1, 3, 2, 4 };
+        int[] arr2 = { 4, 5, 2, 10, 8 };
         int[] result1 = nextGreaterElement(arr1);
         int[] result2 = nextGreaterElement(arr2);
 
