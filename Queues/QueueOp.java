8b80de3d2d3e13ffe81732fb16d170b5aa10c6a2
Mohit kumar <mohitsinghoi501@gmail.com>
2026-04-19 23:28:42 +0530

    initial commit

---
+++ Queues/QueueOp.java
@@ -0,0 +1,70 @@
+// WAP in Queue to add remove and display the elements of the queue.
+
+public class QueueOp {
+    static class Queue {
+        int arr[];
+        int size;
+        int rear;
+
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+
+        public boolean isEmpty() {
+            return rear == -1;
+        }
+
+        // add
+        public void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Queue is full");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        // remove
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        // Peek
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6); // Queue is full
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+
+}
