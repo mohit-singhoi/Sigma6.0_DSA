1866fe48c64113b391fce627bfd8aa84c6d4d12d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-04-22 00:08:44 +0530

    initial commit

--- Queues/QueueJCF.java
+++ Queues/QueueJCF.java
@@ -3,7 +3,8 @@
 import java.util.*;
 public class QueueJCF {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+       // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
