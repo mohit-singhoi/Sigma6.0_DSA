9edc90f8b7da733ea0934b31f2187081d93b3e4f
Mohit kumar <mohitsinghoi501@gmail.com>
2026-04-23 00:10:53 +0530

    initial commit

---
+++ Queues/DeQueOp.java
@@ -0,0 +1,35 @@
+// WAP to implement DeQue operations in JCF.
+import java.util.*;
+public class DeQueOp {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new LinkedList<>();
+
+        // addFirst() : O(1)
+        dq.addFirst(1);
+        dq.addFirst(2);
+        dq.addFirst(3); // Deque : [3, 2, 1]
+
+        // addLast() : O(1)
+        dq.addLast(4); 
+        dq.addLast(5);
+        dq.addLast(6); // Deque : [3, 2, 1, 4, 5, 6]
+
+        System.out.println("Deque : " + dq);
+
+        // removeFirst() : O(1)
+        System.out.println("Removed from front : " + dq.removeFirst());
+
+        // removeLast() : O(1)
+        System.out.println("Removed from rear : " + dq.removeLast());
+
+        System.out.println("Deque after removals : " + dq);
+
+        // peekFirst() : O(1)
+        System.out.println("Front element : " + dq.peekFirst());
+
+        // peekLast() : O(1)
+        System.out.println("Rear element : " + dq.peekLast());
+
+    }
+    
+}
