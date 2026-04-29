a53fa6906b19661acc1babb478d1c5549e0eeb43
Mohit kumar <mohitsinghoi501@gmail.com>
2026-04-29 17:25:02 +0530

    initial commit

---
+++ Tree/Binary Tree/BinaryTreePre.java
@@ -0,0 +1,53 @@
+// Binary Tree Preorder Traversal
+public class BinaryTreePre {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    // Preorder Traversal
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+public static void main(String[] args) {
+    int nodes[] = {1, 2, 4, -1, -1, -1, 3, -1, 6, -1, -1};
+
+    Node root = BinaryTree.buildTree(nodes);
+
+    System.out.println("Root Node: " + root.data);
+
+    System.out.print("Preorder Traversal: ");
+   preorder(root);
+}
+}
