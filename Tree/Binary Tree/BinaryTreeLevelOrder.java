2f72065ffb1f4d46b28a2aca58732126d233ca79
Mohit kumar <mohitsinghoi501@gmail.com>
2026-05-01 23:27:29 +0530

    initial commit

---
+++ Tree/Binary Tree/BinaryTreeLevelOrder.java
@@ -0,0 +1,77 @@
+// Level Order Traversal of Binary Tree
+
+import java.util.*;
+public class BinaryTreeLevelOrder {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    // Level Order Traversal
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+
+            if (currNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) { // O(n) time complexity
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        Node root = BinaryTree.buildTree(nodes);
+
+        System.out.println("Root Node: " + root.data);
+
+        System.out.println("Level Order Traversal: ");
+        levelOrder(root);
+    }
+    
+}
---
+++ Tree/Binary Tree/HeightOfTree.java
@@ -0,0 +1,46 @@
+// Height of a binary tree is the number of edges on the longest path from the root to a leaf node using recursion.
+public class HeightOfTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Height of the tree
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    // Main class to build the tree
+    public static void main(String[] args) {
+        
+    /*                   1
+                        /  \
+                       2    3
+                      / \  / \
+                     4   5 6  7
+             
+   */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println("Height of the tree is: " + height(root));
+
+    }
+    
+}
