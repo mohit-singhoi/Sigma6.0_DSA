30e36d190267b8009c379479f4ca7c67ddf9e99c
Mohit kumar <mohitsinghoi501@gmail.com>
2026-05-21 23:34:00 +0530

    initail commit

---
+++ Heaps/NRopesProblem.java
@@ -0,0 +1,5 @@
+// Connect N Ropes with minimum cost
+// Given are N ropes of different lengths, we need to connect these ropes into one rope. The cost to connect two ropes is equal to the sum of their lengths. We need to connect the ropes with minimum cost.
+public class NRopesProblem {
+    
+}
