2d66a186912b1255e569be30674e6f131cdcb7a9
Mohit kumar <mohitsinghoi501@gmail.com>
2026-05-22 13:48:12 +0530

    initial commit

--- Heaps/NRopesProblem.java
+++ Heaps/NRopesProblem.java
@@ -1,5 +1,27 @@
 // Connect N Ropes with minimum cost
 // Given are N ropes of different lengths, we need to connect these ropes into one rope. The cost to connect two ropes is equal to the sum of their lengths. We need to connect the ropes with minimum cost.
+
+import java.util.PriorityQueue;
 public class NRopesProblem {
-    
+    public static void main(String[] args) {
+        int[] ropes = { 4, 3, 2, 6 };
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int rope : ropes) {
+            minHeap.add(rope);
+        }
+
+        int cost = 0;
+        while (minHeap.size() > 1) {
+            int first = minHeap.poll();
+            int second = minHeap.poll();
+
+            int combined = first + second;
+            cost += combined;
+
+            minHeap.add(combined);
+        }
+        System.out.println("Minimum cost to connect ropes: " + cost);
+    }
+
 }
