48caaabfea05205e9e6f7006ad25883e4ddea4ab
Mohit kumar <mohitsinghoi501@gmail.com>
2026-05-23 23:53:58 +0530

    initial commit

---
+++ Hashing/HMapIteration.java
@@ -0,0 +1,30 @@
+// Iterations on HashMap
+
+import java.util.*;
+public class HMapIteration {
+    public static void main(String[] args) {// O(n) for iteration
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("India", 150);
+        map.put("USA", 45);
+        map.put("China", 140);
+        map.put("Russia", 30);
+        map.put("Japan", 20);
+        map.put("Germany", 25);
+
+        //Iterate
+        Set<String> keys = map.keySet();
+        System.out.println("Keys: " + keys);
+
+        // Iterate using for each loop
+        System.out.println("Iterating using for each loop:");
+        System.out.println("Key : Value");
+        for (String key : keys) {
+            System.out.println(key + " : " + map.get(key));
+        }
+
+       
+    }
+    
+}
