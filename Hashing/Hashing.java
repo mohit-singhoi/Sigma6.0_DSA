5ee480eb67d3b1b9970ddd9060389b2962074792
Mohit kumar <mohitsinghoi501@gmail.com>
2026-05-23 22:58:15 +0530

    initial commit

---
+++ Hashing/Hashing.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Hashing{
+    public static void main(String[] args) {
+
+        HashMap<Integer,String> map = new HashMap<>();
+
+        map.put(101,"Mohit");
+        map.put(102,"Rahul");
+        map.put(103,"Aman");
+
+        System.out.println(map);
+    }
+}
