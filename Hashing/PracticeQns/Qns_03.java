6a431d1d9bf717435908cfe58f97ab252ac6eba5
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-06 22:56:31 +0530

    initial commit

---
+++ Hashing/PracticeQns/Qns_03.java
@@ -0,0 +1,23 @@
+// Qns_03 : Bottom View of a Binary Tree
+// The top view of a binary tree is the set of nodes visible when the tree is viewed from the top.
+// given a binary tree, print the top view of it. The output nodes can be printed in any order.
+
+// Sample input 
+    //      20
+    //     /  \
+    //    8   22
+    //  /  \   \
+    // 5    3   25
+    //     / \
+    //    10 14
+
+// Sample Output
+// 5,10,3,14,25
+// Use the concept of Verticle order    
+
+package PracticeQns;
+
+public class Qns_03 {
+    
+    
+}
