e778099bf665605c8c9a6d0dc8304daec4067bec
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-07 23:37:32 +0530

    initial commit

---
+++ Trie/CreateTrie.java
@@ -0,0 +1,23 @@
+// This is a Java implementation of a Trie (also known as a Prefix Tree).
+
+public class CreateTrie {
+
+    public static Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node(){
+            for(int i=0;i<26;i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+    
+    public static void main(String [] args){
+        String words[] = {"the" , "a" , "there", "their", "any" , "thee"};
+
+    }
+    
+}
