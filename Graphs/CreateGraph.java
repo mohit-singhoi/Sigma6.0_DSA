6bc78d70b743942bd71d51fc8521d5d6ff5db886
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-15 23:47:46 +0530

    initial commit

--- Graphs/CreateGraph.java
+++ Graphs/CreateGraph.java
@@ -1,5 +1,18 @@
 // create a graph using adjacency list representation.
 public class CreateGraph {
+
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+
+    }
     public static void main(String[] args) {
         /*
                    (5)
