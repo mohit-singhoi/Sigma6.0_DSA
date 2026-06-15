d43035abc830540dac68521eff53f0099a4a642a
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-15 23:41:51 +0530

    initial commit

---
+++ Graphs/CreateGraph.java
@@ -0,0 +1,19 @@
+// create a graph using adjacency list representation.
+public class CreateGraph {
+    public static void main(String[] args) {
+        /*
+                   (5)
+                 0 --------1
+                          / \
+                    (1) /     \ (3)
+                       /       \
+                       2--------3  
+                       |    (1)
+                   (2) |
+                       |
+                       4
+
+         */
+    }
+    
+}
