602dfb94012a5456fc797af34302e1926d163917
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-18 00:17:06 +0530

    initial commit

--- Graphs/DetectCycle.java
+++ Graphs/DetectCycle.java
@@ -22,13 +22,13 @@ public class DetectCycle {
         }
 
         graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
+     //   graph[0].add(new Edge(0, 2));
         graph[0].add(new Edge(0, 3));
 
         graph[1].add(new Edge(1, 0));
         graph[1].add(new Edge(1, 2));
 
-        graph[2].add(new Edge(2, 0));
+      //  graph[2].add(new Edge(2, 0));
         graph[2].add(new Edge(2, 1));
 
         graph[3].add(new Edge(3, 0));
@@ -37,7 +37,7 @@ public class DetectCycle {
         graph[4].add(new Edge(4, 3));
     }
 
-    // Detect Cycle
+    // Detect Cycle : O(V+E)
     public static boolean detectCycle(ArrayList<Edge>[] graph) {
 
         boolean visited[] = new boolean[graph.length];
