84039aa1cdffd417024fe4aa833631667817a12d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-17 23:48:24 +0530

    initial commit

---
+++ Graphs/DetectCycle.java
@@ -0,0 +1,74 @@
+// Check if cycle exists in a undirected graph using DFS.
+
+import java.util.ArrayList;
+
+public class DetectCycle {
+    static class Edge {
+        int src;
+        int dest;
+
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 4));
+
+
+        graph[4].add(new Edge(4, 3));
+
+    }
+
+    // Detect cycle function
+    public static boolean detectCycle(ArrayList<Edge>[] graph){
+        boolean visisted [] = new boolean[graph.length];
+        for(int i=0;i<graph.length;i++){
+            if(!visisted[i]){
+                detectCycleUtil(graph, visisted, i , -1);
+            }
+        }
+    }
+
+    //util function
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph,boolean visited[], int curr, int par){
+
+    }
+
+    public static void main(String[] args){
+        /*
+                 0-----------3
+               / |           |
+             /   |           |
+            /    |           | 
+           1     |           4
+            \    |         
+             \   |
+               \ 2
+        
+        */
+
+        int V = 4;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+    }
+    
+}
