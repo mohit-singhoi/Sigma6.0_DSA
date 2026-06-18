d66a8cba7958869af9fc78308611706e9f3acc6b
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-18 22:51:53 +0530

    initial commit

--- Graphs/Part_2/BipartiteGraph.java
+++ Graphs/Part_2/BipartiteGraph.java
@@ -3,6 +3,8 @@
 // In other words, for every edge(u,v), either u belongs to U ans v belongs to V or u belongs to V and v belongs to U. We can also say that there is no edge that connects vertices of same set.
 
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class BipartiteGraph {
     static class Edge {
@@ -32,17 +34,63 @@ public class BipartiteGraph {
         graph[2].add(new Edge(2, 4));
 
         graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
+        //graph[3].add(new Edge(3, 4));
 
         graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
+        //graph[4].add(new Edge(4, 3));
+    }
+
+
+    // O(V+E)
+    public static boolean isBipartite(ArrayList<Edge>[] graph){
+        int col[] = new int [graph.length];
+        for(int i = 0;i<col.length;i++){
+            col[i] = -1; // no color
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=0;i<graph.length;i++){
+            if(col[i] ==  -1){ // BFS
+                q.add(i);
+                col[i] = 0; // yellow
+                while(!q.isEmpty()){
+                    int curr = q.remove();
+                    for(int j = 0; j<graph[curr].size();j++){
+                        Edge e = graph[curr].get(j); // e.dest
+                        if(col[e.dest] == -1){
+                            int nextColor = col[curr] == 0 ? 1 :0;
+                            col[e.dest] = nextColor;
+                            q.add(e.dest);
+                        }
+                        else if(col[e.dest] == col[curr]){
+                            return false; // NOT Bipartite
+                        }
+                    }
+                }
+
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         /*
-        
-        
+                0 -------2
+               /        /
+              /        /
+             1        4
+              \      /
+               \    /
+                  3
+                FALSE
+               
         */
+
+        int V = 5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        System.out.print("Is Bipartite Graph ? : " + isBipartite(graph));
     }
 
 }
