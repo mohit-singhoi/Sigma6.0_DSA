d237f87640be8002f96ac4d00636d56dd971b405
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-23 00:00:12 +0530

    initial commit

--- Graphs/Part_3/SourceToTarget.java
+++ Graphs/Part_3/SourceToTarget.java
@@ -20,6 +20,8 @@ public class SourceToTarget {
             graph[i] = new ArrayList<>();
         }
 
+        graph[0].add(new Edge(0,3));
+
         graph[2].add(new Edge(2, 3));
 
         graph[3].add(new Edge(3, 1));
@@ -33,17 +35,28 @@ public class SourceToTarget {
 
     public static void printAllPath(ArrayList<Edge>[] graph, int src, int dest, String path){
         if(src == dest){
-            
+            System.out.println(path+dest);
+            return;
+
+        }
+        for(int i =0;i<graph[src].size();i++){
+            Edge e = graph[src].get(i);
+            printAllPath(graph, e.dest, dest, path+src);
         }
 
     }
 
-    public static void main(STring[] args){
+    public static void main(String[] args){
         int V = 6;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
 
         int src = 5, dest = 1;
+
+        System.out.println("Src to Dest Path : ");
+        printAllPath(graph, src, dest, "");
+
+       
 
 
     }
