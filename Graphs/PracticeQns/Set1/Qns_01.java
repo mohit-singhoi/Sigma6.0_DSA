65d713fd128de35faeddf241d0516b3c142a659c
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-23 01:31:58 +0530

    initial commit

---
+++ Graphs/PracticeQns/Set1/Qns_01.java
@@ -0,0 +1,15 @@
+// Qns_01 : Detect cycle in an undirected graph using BFS.
+
+// We have an undirected graph , how to check it there is a cycle in the graph? For example, the following graph has a cycle 1-0-2-1..
+
+// Input : 
+/* 
+ 9 - 5 - 2
+ |  /    |
+ 1       4
+*/
+
+// Output : Yes
+public class Qns_01 {
+    
+}
