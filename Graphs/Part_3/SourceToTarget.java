c357e51c0390642a89abf419a3acff4613f7f8be
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-22 23:20:52 +0530

    initial commit

--- Graphs/Part_3/SourceToTarget.java
+++ Graphs/Part_3/SourceToTarget.java
@@ -1,4 +1,50 @@
 // All Path from  Source to Taget Problem using Directed Graph
+
+import java.util.ArrayList;
+
 public class SourceToTarget {
-    
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    // Create Graph
+    static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+
+    public static void printAllPath(ArrayList<Edge>[] graph, int src, int dest, String path){
+        if(src == dest){
+            
+        }
+
+    }
+
+    public static void main(STring[] args){
+        int V = 6;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        int src = 5, dest = 1;
+
+
+    }
 }
