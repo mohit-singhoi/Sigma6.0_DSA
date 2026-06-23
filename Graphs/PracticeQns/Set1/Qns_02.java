2a579323019d4061b3fd4f33972fde7199604f90
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-23 23:41:07 +0530

    initial commit

---
+++ Graphs/PracticeQns/Set1/Qns_02.java
@@ -0,0 +1,19 @@
+// Qns_02 : Find Minimum depth of binary tree Problem
+// we have a binary tree , find its minimum depth. The minimum depth is the number of nodes along the sortest path from the root node down to the nearest leaf node.
+// For example , minimum height of below binary tree is 2
+
+// Sample input 1
+
+/*      1
+       /  \
+      8    2
+    / \    /
+   6  5   9
+
+*/
+
+// Sample output : 2
+
+public class Qns_02 {
+    
+}
