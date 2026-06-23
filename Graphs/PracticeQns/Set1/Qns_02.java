31f2479559cbdbf05bdc1130afc00cabc45d05fd
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-23 23:45:19 +0530

    initail commit

--- Graphs/PracticeQns/Set1/Qns_02.java
+++ Graphs/PracticeQns/Set1/Qns_02.java
@@ -12,8 +12,97 @@
 
 */
 
-// Sample output : 2
+// Sample output : 3
+
+import java.util.*;
 
 public class Qns_02 {
-    
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+
+    static int minDepth(Node root) {
+
+        if (root == null)
+            return 0;
+
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        int depth = 1;
+
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+
+            for (int i = 0; i < size; i++) {
+
+                Node curr = q.remove();
+
+
+                // leaf node found
+                if (curr.left == null && curr.right == null) {
+                    return depth;
+                }
+
+
+                if (curr.left != null)
+                    q.add(curr.left);
+
+
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+
+
+            depth++;
+        }
+
+
+        return depth;
+    }
+
+
+
+    public static void main(String args[]) {
+
+
+        /*
+                 1
+               /   \
+              8     2
+             / \    /
+            6   5  9
+
+        */
+
+
+        Node root = new Node(1);
+
+        root.left = new Node(8);
+        root.right = new Node(2);
+
+        root.left.left = new Node(6);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(9);
+
+
+
+        System.out.print("Minimum Depth of Binary Tree is : ");
+        System.out.print(minDepth(root));
+    }
 }
