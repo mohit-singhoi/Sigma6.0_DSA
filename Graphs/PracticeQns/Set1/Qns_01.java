4f94b6b695f55ab8d0bec71a545594b6baccdf65
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-23 23:34:25 +0530

    initial commit

--- Graphs/PracticeQns/Set1/Qns_01.java
+++ Graphs/PracticeQns/Set1/Qns_01.java
@@ -10,6 +10,108 @@
 */
 
 // Output : Yes
+
+import java.util.*;
+
 public class Qns_01 {
-    
+
+    static class Edge {
+        int src;
+        int dest;
+
+        Edge(int s, int d) {
+            src = s;
+            dest = d;
+        }
+    }
+
+    static boolean detectCycle(ArrayList<Edge>[] graph, int V) {
+
+        boolean visited[] = new boolean[V];
+
+        // check for disconnected components
+        for (int i = 0; i < V; i++) {
+
+            if (!visited[i]) {
+
+                if (bfs(graph, i, visited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    static boolean bfs(ArrayList<Edge>[] graph, int start, boolean visited[]) {
+
+        Queue<int[]> q = new LinkedList<>();
+
+        // node, parent
+        q.add(new int[]{start, -1});
+        visited[start] = true;
+
+
+        while (!q.isEmpty()) {
+
+            int curr[] = q.remove();
+
+            int node = curr[0];
+            int parent = curr[1];
+
+
+            for (Edge e : graph[node]) {
+
+                int neighbour = e.dest;
+
+
+                if (!visited[neighbour]) {
+
+                    visited[neighbour] = true;
+                    q.add(new int[]{neighbour, node});
+
+                } 
+                else if (neighbour != parent) {
+
+                    // visited node is not parent => cycle
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String args[]) {
+
+        int V = 5;
+
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        for(int i=0;i<V;i++){
+            graph[i] = new ArrayList<>();
+        }
+
+
+        // 0--1
+        // |  |
+        // 2--
+
+        graph[0].add(new Edge(0,1));
+        graph[1].add(new Edge(1,0));
+
+        graph[1].add(new Edge(1,2));
+        graph[2].add(new Edge(2,1));
+
+        graph[2].add(new Edge(2,0));
+        graph[0].add(new Edge(0,2));
+
+
+        if(detectCycle(graph,V))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
 }
