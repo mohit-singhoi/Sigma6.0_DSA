3a85ec6ca880ef8f8c1a1b42c21eac713971557f
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-24 23:40:00 +0530

    initial commit

--- Graphs/Part_4/BellmanFordAlgo.java
+++ Graphs/Part_4/BellmanFordAlgo.java
@@ -1,5 +1,41 @@
 // Bellman Ford Algorithm
 
+import java.util.ArrayList;
+
 public class BellmanFordAlgo {
+
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    // Edge[]
+    static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0,1,2));
+        graph[0].add(new Edge(0,2,4));
+
+        graph[1].add(new Edge(1,2,-4));
+
+        graph[2].add(new Edge(2, 3,2));
+
+        graph[3].add(new Edge(3, 4,4));
+
+    }
+
+    public static void main(String[] args[]){
+        
+    }
     
 }
