85e55824e31927491fa00977b47a7105a28e09e5
Mohit kumar <mohitsinghoi501@gmail.com>
2026-06-26 23:52:05 +0530

    initial commit

---
+++ Graphs/Part_5/CheapestFlightK.java
@@ -0,0 +1,9 @@
+// Cheapest Flight within K Stops Problem
+
+// There are n cities connected by some number of flights. You are given an array flights where flights[i] = [from, to, price] indicates that there is a flight.
+//You have also given three integers src, dst, and k, return cheapest price from src to dst with atmost k stops. if there is no such route , return -1;
+// given all values are postive
+
+public class CheapestFlightK {
+    
+}
