3fe71b3c64faea893e6d09da1765be16ae11ee92
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-02 22:27:58 +0530

    initial commit

---
+++ Graphs/Part_5/FloodFillAlgo.java
@@ -0,0 +1,12 @@
+// Flood Fill Algorithm
+// Given a m x n integer grid image where image[i][j] represents the pixel value of the image.
+// You are also given three integers sr , sc, and color. You should perform a flood fillon the image starting from the pixel image[sr][sc].
+// To perform a flood fill, consider the starting pixel, plus any pixels connected 4-dirctinally to the starting pixel of the same  color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also withthe same color), and so on. Replace the color of all the aforementioned pixels with color.
+
+// image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+
+// Output: [[2,2,2],[2,2,0],[2,0,1]]
+
+public class FloodFillAlgo {
+    
+}
