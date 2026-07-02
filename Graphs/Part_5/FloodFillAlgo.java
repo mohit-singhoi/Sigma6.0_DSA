7bad8b290fd2d3b0fefb4c3d97b3e39c27f0d91c
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-02 23:48:18 +0530

    initial commit

--- Graphs/Part_5/FloodFillAlgo.java
+++ Graphs/Part_5/FloodFillAlgo.java
@@ -13,6 +13,9 @@
 // up(sr-1, sc)
 // down(sr+1, sc) 
 
+// Def : Floyd-Warshall Algorithm is an algorithm for finding the shortest path between all the pairs of vertices in a weighted graph. This algorithm works for both the directed and undirected weighted graphs. 
+// But, it does not work for the graphs with negative cycles (where the sum of the edges in a cycle is negative).
+
 public class FloodFillAlgo {
 
     public void helper(int[][] image, int sr, int sc,
