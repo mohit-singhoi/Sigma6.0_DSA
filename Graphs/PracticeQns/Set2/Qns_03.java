35ccebbc8b57a39b1c89f89003b0040dc16e26e9
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-04 00:00:10 +0530

    initial commit

--- Graphs/PracticeQns/Set2/Qns_03.java
+++ Graphs/PracticeQns/Set2/Qns_03.java
@@ -1,4 +1,11 @@
 // Qns_03.java
+// Find whether it is possible to finish all tasks or not
+
+// There are a total of n tasks you have to pick, labelled from 0 to n − 
+// 1. Some tasks may have prerequisites; for example, to pick task 0, you have to first pick task 1, which is expressed as a pair: [0, 1]
+
+// Given the total number of tasks and a list of prerequisite pairs, determine whether it is possible for you to finish all the tasks.
+
 public class Qns_03 {
     
 }
