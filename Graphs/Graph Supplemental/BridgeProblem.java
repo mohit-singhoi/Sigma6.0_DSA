46105216aadb057a25e3173f12209e09aee84cf9
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-04 23:52:28 +0530

    initial commit

---
+++ Graphs/Graph Supplemental/BridgeProblem.java
@@ -0,0 +1,7 @@
+// Bridge in a Graph : It is an edge whose removal increases the number of connected components in the graph.
+// In other words, a bridge is an edge that, if removed, would disconnect the graph into two or more separate components.
+// Bridges are important in network analysis and graph theory because they represent critical connections that, if disrupted, can lead to the fragmentation of the network.
+
+public class BridgeProblem {
+    
+}
