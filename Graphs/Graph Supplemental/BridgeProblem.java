5581e0764a1189df68241c98920931bca74d6e57
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-05 23:53:57 +0530

    initial commit

--- Graphs/Graph Supplemental/BridgeProblem.java
+++ Graphs/Graph Supplemental/BridgeProblem.java
@@ -7,7 +7,7 @@
 import java.util.ArrayList;
 
 public class BridgeProblem {
-    stack class Edge{
+    static class Edge {
         int src;
         int dest;
 
@@ -17,64 +17,76 @@ public class BridgeProblem {
         }
     }
 
-    //Create graph
+    // Create graph
     public static void createGraph(ArrayList<Edge>[] graph) {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        // Add edges to the graph
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
-        graph[0].add(new Edge(0, 3));
+    // 0 - 1
+    graph[0].add(new Edge(0, 1));
+    graph[1].add(new Edge(1, 0));
 
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 2));
+    // 0 - 2
+    graph[0].add(new Edge(0, 2));
+    graph[2].add(new Edge(2, 0));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
+    // 1 - 2
+    graph[1].add(new Edge(1, 2));
+    graph[2].add(new Edge(2, 1));
 
-        graph[3].add(new Edge(3, 0));
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
+    // 0 - 3
+    graph[0].add(new Edge(0, 3));
+    graph[3].add(new Edge(3, 0));
 
-        graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
+    // 3 - 4
+    graph[3].add(new Edge(3, 4));
+    graph[4].add(new Edge(4, 3));
+
+    // 4 - 5
+    graph[4].add(new Edge(4, 5));
+    graph[5].add(new Edge(5, 4));
+
+    // 3 - 5
+    graph[3].add(new Edge(3, 5));
+    graph[5].add(new Edge(5, 3));
     }
 
-    public static void dfs(ArrayList<Edge> graph[], int curr, int par, int dt[], int low[], boolean vis[], int time){
+    static int time = 0;
+    public static void dfs(ArrayList<Edge> graph[], int curr, int par,
+            int dt[], int low[], boolean vis[]) {
         vis[curr] = true;
         dt[curr] = low[curr] = ++time;
 
-        for(int i=0;i<graph[curr].size();i++){
-            Edge e = graph[curr].get(i);
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i); // e.src --e.dest
             int nbr = e.dest;
 
-            if(nbr == par){
+            if (nbr == par) {
                 continue;
-            }else if(!vis[nbr]){
-                dfs(graph,nbr,curr,dt,low,vis,time);
-                low[curr] = Math.min(low[curr],low[nbr]);
+            } else if (!vis[nbr]) {
+                dfs(graph, nbr, curr, dt, low, vis);
+                low[curr] = Math.min(low[curr], low[nbr]);
 
-                //Check for bridge
-                if(dt[curr]<low[nbr]){
-                    System.out.println("Bridge : "+curr+" -> "+nbr);
+                // Check for bridge
+                if (dt[curr] < low[nbr]) {
+                    System.out.println("Bridge : " + curr + " ---> " + nbr);
                 }
-            }else{
-                low[curr] = Math.min(low[curr],dt[nbr]);
+            } else {
+                low[curr] = Math.min(low[curr], dt[nbr]);
             }
         }
     }
 
-    public static void tarjanBridge(ArrayList<Edge> graph[], int V){
+    public static void tarjanBridge(ArrayList<Edge>[] graph, int V) {
+
         int dt[] = new int[V];
         int low[] = new int[V];
-        int time = 0;
         boolean vis[] = new boolean[V];
 
-        for(int i =0;i<V;i++){
-            if(!vis[i]){
-                dfs(graph,i,-1,dt,low,vis,time);
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(graph, i, -1, dt, low, vis);
             }
         }
     }
@@ -85,7 +97,8 @@ public class BridgeProblem {
         createGraph(graph);
 
         // Call the function to find bridges in the graph
-        findBridges(graph, vertices);
+        System.out.println("Bridges in the graph:");
+        tarjanBridge(graph, vertices);
     }
-    
+
 }
