f60b69b7c6420c11e9332ee716988876746ca246
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-09 00:34:58 +0530

    initial commit

--- Dynamic Programming/Part2/ZeroKnapsack.java
+++ Dynamic Programming/Part2/ZeroKnapsack.java
@@ -1,10 +1,11 @@
-// 0 - 1 Knapsack Problem
+// 0 - 1 Knapsack Problem using Recusrsion
 // Val[] = 15, 14, 10, 45, 30
 // wt[] = 2, 5, 1, 3, 4
 // W(total allowed weight) = 7
 // ans = max Profit
 public class ZeroKnapsack {
 
+    // O(2^n)
     public static int kanpsack(int val[], int wt[], int W, int n) {
         if (W == 0 || n == 0) {
             return 0;
---
+++ Dynamic Programming/Part2/Zeroknapsack1.java
@@ -0,0 +1,26 @@
+// 0 - 1 Kanpsack problem using DP(Memoization)
+// Val[] = 15, 14, 10, 45, 30
+// wt[] = 2, 5, 1, 3, 4
+// W(total allowed weight) = 7
+// ans = max Profit
+public class Zeroknapsack1 {
+
+
+
+    public static void main(String[] args) {
+        int val[] = { 15, 14, 10, 45, 30 };
+        int wt[] = { 2, 5, 1, 3, 4 };
+        int W = 7;
+        int dp[][] = new int[val.length+1][W+1];
+        for(int i =0;i<dp.length;i++){
+            for(int j =0;j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+            
+        }
+
+        System.out.println("Max Profit : " + kanpsack(val, wt, W, val.length,dp));
+
+    }
+
+}
