5533c76c3cd08a3571ce83fb0b4f277700741ed7
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-09 23:28:09 +0530

    initial commit

--- Dynamic Programming/Part2/ZeroOneKnapsack2.java
+++ Dynamic Programming/Part2/ZeroOneKnapsack2.java
@@ -2,7 +2,8 @@
 
 public class ZeroOneKnapsack2 {
 
-    public static int knapsack(int val[], int wt[], int W) {
+    // O(n * W)
+    public static int knapsackTab(int val[], int wt[], int W) {
         int n = val.length;
         int dp[][] = new int[n + 1][W + 1];
         for (int i = 0; i < dp.length; i++) { // 0th col
@@ -27,7 +28,20 @@ public class ZeroOneKnapsack2 {
                 }
             }
         }
+        print(dp);
         return dp[n][W];
+    }
+
+    //Print dp
+    public static void print(int dp[][]){
+        for(int i =0;i<dp.length;i++){
+            for(int j =0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+         System.out.println();
+        
     }
 
     public static void main(String[] args) {
@@ -36,7 +50,7 @@ public class ZeroOneKnapsack2 {
         int wt[] = { 2, 5, 1, 3, 4 };
         int W = 7;
 
-        System.out.println("Max Profit : " + knapsack(val, wt, W));
+        System.out.println("Max Profit : " + knapsackTab(val, wt, W));
     }
 
 }
