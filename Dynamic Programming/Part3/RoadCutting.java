4480e7d8cf19ca4bc04c3deed8d3f6c73489ec05
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-11 18:41:40 +0530

    initial commit

--- Dynamic Programming/Part3/RoadCutting.java
+++ Dynamic Programming/Part3/RoadCutting.java
@@ -1,5 +1,10 @@
 // Road Cutting Problem 
+// Given a rod of length n inch and an array of prices that includes prices of all prices of size smalleer than n. 
+// Determine the maximum value obtainable by cutting up the rod and selling the prices.
 
+// length =  1, 2, 3, 4, 5, 6, 7, 8
+// price = 1, 5, 8, 9, 10, 17, 17, 20
+// rodLength = 8
 public class RoadCutting {
     
 }
