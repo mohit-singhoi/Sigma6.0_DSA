ab4bea83cf539e7f00c2b384c651380cd5b350ef
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-15 17:47:55 +0530

    initial commit

--- Dynamic Programming/Part3/RoadCutting.java
+++ Dynamic Programming/Part3/RoadCutting.java
@@ -21,7 +21,7 @@ public class RoadCutting {
         }
 
         for(int i =1;i<n+1;i++){
-            for(int j =1;j<totRod;j++){
+            for(int j =1;j<=totRod;j++){
                 //valid
                 if(length[i-1] <=j){
                     dp[i][j] = Math.max(price[i-1] + dp[i][j-length[i-1]], dp[i-1][j]);
@@ -37,7 +37,7 @@ public class RoadCutting {
 
     public static void main(String[] args){
         int length[] = {1,2,3,4,5,6,7,8}; // pieces length
-        int price[] = {1,5,8,9,10,17,20};
+        int price[] = {1,5,8,9,10,17,17,20};
         int totRod = 8;
         System.out.println("Max Profit : " + rodCutting(length,price,totRod));
     }
