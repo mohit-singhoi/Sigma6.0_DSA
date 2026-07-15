392047557614bbd7039fd06b43890888b6aa3ee7
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-15 18:03:57 +0530

    initial commit

---
+++ Dynamic Programming/Part3/LCS.java
@@ -0,0 +1,11 @@
+// Longest Common Sequence (LCS) using recursion
+// str1 = "abcde" , str2 = "ace"
+// ans = 3 //"ace"
+
+// str1 = "abcdge", str2 ="abedg"
+// ans = 4 //"abdg"
+
+
+public class LCS {
+    
+}
--- Dynamic Programming/Part3/RoadCutting.java
+++ Dynamic Programming/Part3/RoadCutting.java
@@ -6,40 +6,53 @@
 // price = 1, 5, 8, 9, 10, 17, 17, 20
 // rodLength = 8
 public class RoadCutting {
-    // Weight => length; Val => price; W => totRod
-    public static int rodCutting(int length[], int price[], int totRod){
-        int n = price.length;
-        int dp[][] = new int[n+1][totRod+1];
 
-        for(int i = 0;i<n+1;i++){
-            for(int j =0;j<totRod+1;j++){
-                if(i == 0 || j == 0){
+    // print dp table
+    public static void printDP(int dp[][]) {
+        System.out.println("\nDP Table:");
+
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    // Weight => length; Val => price; W => totRod
+    public static int rodCutting(int length[], int price[], int totRod) {
+        int n = price.length;
+        int dp[][] = new int[n + 1][totRod + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < totRod + 1; j++) {
+                if (i == 0 || j == 0) {
                     dp[i][j] = 0;
                 }
             }
 
         }
 
-        for(int i =1;i<n+1;i++){
-            for(int j =1;j<=totRod;j++){
-                //valid
-                if(length[i-1] <=j){
-                    dp[i][j] = Math.max(price[i-1] + dp[i][j-length[i-1]], dp[i-1][j]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= totRod; j++) {
 
-                } else { // invalid
-                    dp[i][j] = dp[i-1][j];
-
+                if (length[i - 1] <= j) {
+                    dp[i][j] = Math.max(price[i - 1] + dp[i][j - length[i - 1]],dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return dp[n][totRod];
+
+        printDP(dp); // Print DP table
+        return dp[n][totRod]; // Return maximum profit
     }
 
-    public static void main(String[] args){
-        int length[] = {1,2,3,4,5,6,7,8}; // pieces length
-        int price[] = {1,5,8,9,10,17,17,20};
+    public static void main(String[] args) {
+        int length[] = { 1, 2, 3, 4, 5, 6, 7, 8 }; // pieces length
+        int price[] = { 1, 5, 8, 9, 10, 17, 17, 20 };
         int totRod = 8;
-        System.out.println("Max Profit : " + rodCutting(length,price,totRod));
+        System.out.println("Max Profit : " + rodCutting(length, price, totRod));
     }
-    
+
 }
