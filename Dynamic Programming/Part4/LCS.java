ab8814b9d82cf1338fab3750a33aec2b57e4b55d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-16 23:30:09 +0530

    initial commit

---
+++ Dynamic Programming/Part4/LCS.java
@@ -0,0 +1,7 @@
+// Longest Common Substring
+// A substring is a contiguous sequence of character of characters within a string.
+// S1 = "ABCDE" S2 = "ABGCE"
+// ans = 2 // "AB"
+public class LCS {
+    
+}
