d3b10342da9e5be460212dcb9b294725e80e9c17
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-16 23:55:39 +0530

    initial commit

--- Dynamic Programming/Part4/LCS.java
+++ Dynamic Programming/Part4/LCS.java
@@ -3,6 +3,7 @@
 // S1 = "ABCDE" S2 = "ABGCE"
 // ans = 2 // "AB"
 public class LCS {
+    // O(n*m)
     public static int longestCommonSubstring(String str1, String str2) {
         int n = str1.length();
         int m = str2.length();
@@ -23,6 +24,7 @@ public class LCS {
             for(int j =1;j<m+1;j++){
                 if(str1.charAt(i-1) == str2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1]+1;
+                    ans = Math.max(ans, dp[i][j]);
                 } else {
                     dp[i][j] = 0;
                 }
@@ -30,11 +32,14 @@ public class LCS {
             }
 
         }
+        return ans;
     }
 
     public static void main(String[] args) {
         String str1 = "ABCDE";
         String str2 = "ABGCE";
+
+        System.out.println("Longest Common Substring : " +longestCommonSubstring(str1,str2));
     }
 
 }
