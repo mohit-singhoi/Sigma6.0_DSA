eba05967f035dab9cf46c02c953821babc5aa14f
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-19 17:07:40 +0530

    initial commit

--- Dynamic Programming/Part5/WildcardMatching.java
+++ Dynamic Programming/Part5/WildcardMatching.java
@@ -1,4 +1,19 @@
 // WildCard matching Problem
+//given a text and a wild card pattern , implement wildcard matching algorithm that finds if wildcard pattern is matched with text.
+// The matching should cover the entire text(not partial text).
+// Wild card pattern can include the characters  '?' and '*'
+
+// '?' : Match any single character
+// '*' : matches any sequence of characters (including the empty sequence)
+
+//Ex :
+// Text = "baaabab"
+// Pattern = "*****ba*****ab"
+// Output : true
+
+// Text = "baaabab"
+// Pattern = "a*ab"
+// False
 
 public class WildcardMatching {
     
