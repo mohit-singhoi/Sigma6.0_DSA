6bfdab2e360afaccc20e0e6862b5ec460c6bb902
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-20 01:31:07 +0530

    initial commit

---
+++ Dynamic Programming/Part5/MountainRange.java
@@ -0,0 +1,32 @@
+// Mountain Range Problem
+// Mountains & Valleys at any moment the number of down strokes cannot be more than number of up strokes.
+// Count total possible mountain ranges.
+// Pairs = 3 
+// Ans = 5
+
+public class MountainRange {
+    //O(n^2)
+    public static int mountainranges(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2;i<n+1;i++){
+            //i pairs -> mountain ranges => Ci
+            for(int j =0; j<i;j++){
+                int inside = dp[j];
+                int outside = dp[i-j-1];
+                dp[i] += inside * outside; // ci = cj * ci-j-1
+            }
+        }
+        //n pairs
+        return dp[n];
+    }
+
+    public static void main(String[] args){
+        int n = 4;// ans = 14
+        System.out.println("Total no of Mountain ranges in " + n + " Pairs : " +mountainranges(n));
+    }
+
+    
+}
