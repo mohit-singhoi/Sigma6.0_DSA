4ccb234a1bff4d0dff6197b976df405a26133029
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-20 00:58:39 +0530

    initial commit

--- Dynamic Programming/Part5/CountingTrees.java
+++ Dynamic Programming/Part5/CountingTrees.java
@@ -4,5 +4,25 @@
 // ans = 5
 
 public class CountingTrees {
+    public static int countBSt(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i =2;i<n+1;i++){
+            //Ci -> BST (i nodes) -> dp[i]
+            for(int j =0;j<i;j++){
+                int left = dp[j];
+                int right = dp[i-j-1];
+                dp[i] += left * right;
+            }
+        }
+        return dp[n];
+    }
     
+    public static void main(String[] args){
+        int n = 4;
+        System.out.println("Total BSTs  : " +countBSt(n));
+
+    }
 }
