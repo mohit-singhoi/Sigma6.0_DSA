07004b4d9e6892db47a72482885cca1b8175dc39
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-20 00:10:11 +0530

    initial commit

--- Dynamic Programming/Part5/CatalanNumber2.java
+++ Dynamic Programming/Part5/CatalanNumber2.java
@@ -10,6 +10,26 @@
 import java.util.*;
 
 public class CatalanNumber2 {
-    
+    public static int catalanTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i =2;i<=n;i++){
+            for(int j =0;j<i;j++){
+                dp[i] += dp[j] * dp[i-j-1]; // Ci = Cj * Ci-j-1
+
+            }
+        }
+
+        return dp[n];
+
+    }
+
+        public static void main(String [] args){
+        int n = 5;
+        System.out.println("Catalan of " + n + " is : " + catalanTab(n));
+    }
+
     
 }
