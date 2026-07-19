4c534e29b748c47742a66cd323f77d4f0dad4784
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-19 22:45:59 +0530

    initial commit

--- Dynamic Programming/Part5/WildcardMatching.java
+++ Dynamic Programming/Part5/WildcardMatching.java
@@ -16,5 +16,34 @@
 // False
 
 public class WildcardMatching {
-    
+    public static boolean isMatch(String s, String p){
+        int n = s.length();
+        int m = p.length();
+
+        boolan dp[][] = new boolean[n+1][m+1];
+
+        //initialize
+        dp[0][0] = true;
+        //pattern = " "
+        for(int i =1;i<n+1;i++){
+            dp[i][0] = false;
+        }
+
+        //s = " "
+        for(int j =1;j<m+1;j++){
+            if(p.charAt(j-1) == '*'){
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+
+        //bottom up
+        
+    }
+
+    public static void main(String args[]) {
+        String s = "baaabab";
+        String p = "*****ba*****ab"; //true
+
+    }
+
 }
