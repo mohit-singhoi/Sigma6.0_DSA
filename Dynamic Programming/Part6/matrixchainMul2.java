133de4fa04146cae3a8b07ebb98cb967c5ad316a
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-21 01:16:32 +0530

    initial commit

--- Dynamic Programming/Part6/matrixchainMul2.java
+++ Dynamic Programming/Part6/matrixchainMul2.java
@@ -24,14 +24,27 @@ public class matrixchainMul2 {
 
             }
         }
+        print(dp);
         return dp[1][n-1];
 
-        
+    }
+
+    //Print the dp
+    public static void print(int dp[][]){
+        for(int i = 0;i<dp.length;i++){
+            for(int j =0;j<dp[0].length;j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
     }
 
     public static void main(String[] args){
         int arr[] = {1,2,3,4,3}; // n = 5
         int n = arr.length;
+        System.out.println("Dp Table ");
         System.out.println("Matrix Chain Multiplication : " + mcmTab(arr));
     }
     
