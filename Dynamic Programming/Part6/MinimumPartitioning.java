49e2bfbf79b482e472271d578113222d9e4c5334
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-21 17:32:25 +0530

    initial commit

--- Dynamic Programming/Part6/MinimumPartitioning.java
+++ Dynamic Programming/Part6/MinimumPartitioning.java
@@ -3,5 +3,27 @@
 // Min diff = 1
 
 public class MinimumPartitioning {
+    public static int minPartition(int arr[]){
+        int n  = arr.length;
+        int sum =0;
+        for(int i = 0;i<arr.length;i++){
+            sum += arr[i];
+        }
+
+        int W = sum/2;
+        int dp[][] = new int [n+1][W+1];
+
+        //Bottom up
+        for(int i =1;i<n+1;i++){
+            for(int j =1;j<W+1;j++){
+                
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int nums[] = {1,6,11,5};
+    }
+    
     
 }
