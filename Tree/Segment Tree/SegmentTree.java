88fc2616b1c6581ccf13d31000a395cb25895c4e
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-24 14:22:24 +0530

    initial commit

--- Tree/Segment Tree/SegmentTree.java
+++ Tree/Segment Tree/SegmentTree.java
@@ -5,7 +5,20 @@ public class SegmentTree{
 
     public static void init(int n){
         tree = new int[4*n];
-        
+
+    }
+
+    public static int buildST(int arr[], int i, int start, int end){
+        if(start == end){
+            tree[i] = arr[start];
+            return arr[start];
+        }
+
+        int mid = (start+end)/2;
+        buildST(arr, 2*i+1, start, mid); // left subtree - 2*i+1
+        buildST(arr, 2*i+2, mid+1, end); //right subtree - 2*i+2
+        tree[i] = tree[2*i+1] + tree[2*i+2];
+        return tree[i];
     }
 
     public static void main(String[] args){
