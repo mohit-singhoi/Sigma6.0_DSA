9288f3d017438a5212cc16ebe49f2d87c737c6db
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-24 14:16:03 +0530

    initial commit

---
+++ Tree/Segment Tree/SegmentTree.java
@@ -0,0 +1,16 @@
+// Segment Tree
+
+public class SegmentTree{
+    static int tree[];
+
+    public static void init(int n){
+        tree = new int[4*n];
+        
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int n = arr.length;
+
+    }
+}
