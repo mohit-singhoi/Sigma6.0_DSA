b6d33d28a91f547104ea2d6f72f45149cd5831ec
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-24 14:26:19 +0530

    initial commit

--- Tree/Segment Tree/SegmentTree.java
+++ Tree/Segment Tree/SegmentTree.java
@@ -24,6 +24,12 @@ public class SegmentTree{
     public static void main(String[] args){
         int arr[] = {1,2,3,4,5,6,7,8};
         int n = arr.length;
+        init(n);
+        buildST(arr, 0, 0,n-1);
+
+        for(int i = 0;i<tree.length;i++){
+            System.out.print(tree[i] + " ");
+        }
 
     }
 }
