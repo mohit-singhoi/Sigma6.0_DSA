6931b7bba1d06501a384599a6db14af116ab969e
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-25 00:20:31 +0530

    initial commit

---
+++ Tree/Segment Tree/MaxMin.java
@@ -0,0 +1,12 @@
+// Max/Min Element Queries
+// Given an arr[] , we have to answer few queries:
+// a. Output max/Min for the subarray[i..j]
+// b. update the elementat idx
+
+// {6,8,-1,2,17,1,3,2,4}
+
+import segmenttree.UpdateOnST;
+
+public class MaxMin {
+    
+}
