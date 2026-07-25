467e8d2b0821baa0660dea5c1dc4670a4182ca69
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-25 22:24:03 +0530

    initial commit

--- Tree/Segment Tree/MaxEleQuery.java
+++ Tree/Segment Tree/MaxEleQuery.java
@@ -5,70 +5,95 @@
 // {6,8,-1,2,17,1,3,2,4}
 
 import java.util.Scanner;
+
 public class MaxEleQuery {
     static int tree[];
 
-    public static void init(int n){
+    public static void init(int n) {
 
-        tree = new int[4*n];
+        tree = new int[4 * n];
     }
 
-    public static void buildTree(int i , int si, int sj, int arr[]){
-        if(si == sj){
+    public static void buildTree(int i, int si, int sj, int arr[]) {
+        if (si == sj) {
             tree[i] = arr[si];
             return;
         }
 
-        int mid = (si+sj)/2;
-        buildTree(2*i+1, si, mid, arr);
-        buildTree(2*i+2, mid+1, sj, arr);
+        int mid = (si + sj) / 2;
+        buildTree(2 * i + 1, si, mid, arr);
+        buildTree(2 * i + 2, mid + 1, sj, arr);
 
-        tree[i] = Math.max(tree[2*i+1], tree[2*i+2]);
+        tree[i] = Math.max(tree[2 * i + 1], tree[2 * i + 2]);
     }
 
-    public static int getMax(int arr[], int qi, int qj){
+    public static int getMax(int arr[], int qi, int qj) {
         int n = arr.length;
-        return getMaxUtil(0,0,n-1,qi,qj);
+        return getMaxUtil(0, 0, n - 1, qi, qj);
     }
 
-    public static int getMaxUtil(int i, int si, int sj, int qi, int qj){
-        if(si > qj || sj < qi){ // no overlap
+    public static int getMaxUtil(int i, int si, int sj, int qi, int qj) {
+        if (si > qj || sj < qi) { // no overlap
             return Integer.MIN_VALUE;
-        } else if (si >= qi && sj <= qj){// complete overlap
+        } else if (si >= qi && sj <= qj) {// complete overlap
             return tree[i];
         } else { // partial overlap
-            int mid = (si + sj)/2;
-            int leftAns = getMaxUtil(2*i+1, si, mid, qi, qj);
-            int rightAns = getMaxUtil(2*i+2, mid+1, sj, qi, qj);
-            return Math.max(leftAns , rightAns);
+            int mid = (si + sj) / 2;
+            int leftAns = getMaxUtil(2 * i + 1, si, mid, qi, qj);
+            int rightAns = getMaxUtil(2 * i + 2, mid + 1, sj, qi, qj);
+            return Math.max(leftAns, rightAns);
 
         }
 
     }
 
-
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-        int arr[] = {6,8,-1,2,17,1,3,2,4};
+    public static void update(int arr[], int idx, int newval) {
+        arr[idx] = newval;
         int n = arr.length;
-        
+        updateUtil(0, 0, n - 1, idx, newval);
+    }
+
+    public static void updateUtil(int i, int si, int sj, int idx, int newval) {
+        if (idx < si || idx > sj) {
+            return;
+        }
+
+        tree[i] = Math.max(tree[i], newval);
+
+        if (si != sj) {
+            int mid = (si + sj) / 2;
+            updateUtil(2 * i + 1, si, mid, idx, newval); // left
+            updateUtil(2 * i + 2, mid + 1, sj, idx, newval); // right
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 6, 8, -1, 2, 17, 1, 3, 2, 4 };
+        int n = arr.length;
+
         init(n);
-        buildTree(0, 0, n-1, arr);
+        buildTree(0, 0, n - 1, arr);
 
         // for(int i =0 ;i<tree.length;i++){
-        //     System.out.print(tree[i] +" ");
+        // System.out.print(tree[i] +" ");
         // }
 
-        
         System.out.print("Enter The number from : ");
         int qi = sc.nextInt();
 
         System.out.print("Enter The number to : ");
         int qj = sc.nextInt();
 
-
         int max = getMax(arr, qi, qj);
-        System.out.println("Max Element between range between  "+ qi + " to " + qj+ " is : " +max);
+        System.out.println("Max Element between range between  " + qi + " to " + qj + " is : " + max); // 17
+
+        update(arr, 2, 20);
+
+        max = getMax(arr, qi, qj);
+        System.out.println("After updation Max Element between range between  " + qi + " to " + qj + " is : " + max); // 20
     }
-    
+
 }
