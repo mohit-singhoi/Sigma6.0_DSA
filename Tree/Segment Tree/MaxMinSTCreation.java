e466b97b9ef865f55a50c6247af409ed669ed4e5
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-25 21:36:37 +0530

    initial commit

--- Tree/Segment Tree/MaxMinSTCreation.java
+++ Tree/Segment Tree/MaxMinSTCreation.java
@@ -1,11 +1,11 @@
-// Max/Min Element Queries
+// Max/Min Segment Tree Creation
 // Given an arr[] , we have to answer few queries:
 // a. Output max/Min for the subarray[i..j]
 // b. update the elementat idx
 
 // {6,8,-1,2,17,1,3,2,4}
 
-public class MaxMinQuery {
+public class MaxMinSTCreation {
     static int tree[];
 
     public static void init(int n){
