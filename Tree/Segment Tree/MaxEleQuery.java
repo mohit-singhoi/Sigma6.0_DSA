81249e8bbf855a72540d05cfff2dfcbb6853e872
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-25 21:44:54 +0530

    initial commit

---
+++ Tree/Segment Tree/MaxEleQuery.java
@@ -0,0 +1,9 @@
+// Max Element Query Problem
+// Given an arr[], we have to answer few queries :
+// a. Output Max of subarray
+// Update the element at idx
+// {6,8,-1,2,17,1,3,2,4}
+
+public class MaxEleQuery {
+    
+}
