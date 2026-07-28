25192cae79e935af3ea5f854f94b2df91071a186
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-28 23:35:53 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MajorityEle.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MajorityEle.java
@@ -1,3 +1,28 @@
+// Majority Element Problem 
+
+import java.util.*;
 public class MajorityEle {
+    public static int findMajorityElement(int nums[]){
+        int n = nums.length;
+        int freq = 0, ans = 0;
+
+        for(int i =0;i<n;i++){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            } else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        int nums[] = {2,2,1,1,1,2,2};
+        System.out.println("Majority Element : "+findMajorityElement(nums));
+        
+
+    }
     
 }
