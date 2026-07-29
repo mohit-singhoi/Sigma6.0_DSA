a829d2204d1c087e8a74a9363dac5f509bf6cfc6
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-29 23:57:50 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MissingRepeatedValue.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MissingRepeatedValue.java
@@ -3,6 +3,26 @@
 // Output: [2,4]
 // Explanation: Number 2 is repeated and number 4 is missing so the answer is [2,4].
 public class MissingRepeatedValue{
+    publi static int findMissingAndrepeatedvalues(int grid[][]){
+        HashSet<Integer> set = new HashSet<>();
+        int n = grid.length;
+        int sq = n*n;
+        
+        int currSum = 0;
+        int ans[] = new int[2];
+        for(int i =0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(set.contains(grid[i][j])){
+                    ans[0] = grid[i][j];
+                } else {
+                    set.add(grid[i][j]);
+                    currSum += grid[i][j];
+                }
+            }
+        }
+        
+    }
+
 
     public static void main(String[] args) {
         int grid[][] = {{1,3},{2,2}}; 
