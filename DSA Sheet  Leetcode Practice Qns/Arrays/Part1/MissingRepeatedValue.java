a6f1e779e546150d4663e9a80dc7dd3ed65f76c5
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-29 17:51:08 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MissingRepeatedValue.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MissingRepeatedValue.java
@@ -1,7 +1,10 @@
 // Find Missing and Repeated value LeetCode Problem : 2965
+// Input: grid = [[1,3],[2,2]]
+// Output: [2,4]
+// Explanation: Number 2 is repeated and number 4 is missing so the answer is [2,4].
 public class MissingRepeatedValue{
 
     public static void main(String[] args) {
-        
+        int grid[][] = {{1,3},{2,2}}; 
     }
 }
