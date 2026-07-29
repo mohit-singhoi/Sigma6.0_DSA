fcebdf0e8c003b5501311766ab6a9e00afda2c51
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-30 00:15:00 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MissingRepeatedValue.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part1/MissingRepeatedValue.java
@@ -2,29 +2,51 @@
 // Input: grid = [[1,3],[2,2]]
 // Output: [2,4]
 // Explanation: Number 2 is repeated and number 4 is missing so the answer is [2,4].
-public class MissingRepeatedValue{
-    publi static int findMissingAndrepeatedvalues(int grid[][]){
+
+// Find Missing and Repeated Value
+// LeetCode 2965
+
+import java.util.*;
+
+public class MissingRepeatedValue {
+
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
         HashSet<Integer> set = new HashSet<>();
+
         int n = grid.length;
-        int sq = n*n;
-        
-        int currSum = 0;
-        int ans[] = new int[2];
-        for(int i =0;i<n;i++){
-            for(int j =0;j<n;j++){
-                if(set.contains(grid[i][j])){
-                    ans[0] = grid[i][j];
+        int totalNumbers = n * n;
+
+        int currentSum = 0;
+        int[] ans = new int[2]; // ans[0] = repeated, ans[1] = missing
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (set.contains(grid[i][j])) {
+                    ans[0] = grid[i][j]; // Repeated value
                 } else {
                     set.add(grid[i][j]);
-                    currSum += grid[i][j];
+                    currentSum += grid[i][j];
                 }
             }
         }
-        
+
+        int totalSum = totalNumbers * (totalNumbers + 1) / 2;
+        ans[1] = totalSum - currentSum; // Missing value
+
+        return ans;
     }
 
-
     public static void main(String[] args) {
-        int grid[][] = {{1,3},{2,2}}; 
+        int[][] grid = {
+            {1, 3},
+            {2, 2}
+        };
+
+        int[] result = findMissingAndRepeatedValues(grid);
+
+        System.out.println("Repeated Value : " + result[0]);
+        System.out.println("Missing Value  : " + result[1]);
+        System.out.println("Answer         : " + Arrays.toString(result));
     }
 }
