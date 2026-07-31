48e9e21839b3dbc5f4104c20f7c61728812980cb
Mohit kumar <mohitsinghoi501@gmail.com>
2026-07-31 22:43:36 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part1/Merge2SortedArray.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part1/Merge2SortedArray.java
@@ -7,9 +7,30 @@
 
 public class Merge2SortedArray{
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) { //O(m+n)
+        int idx = m+n-1, i = m-1, j = n-1;
+
+        while(i >= 0 && j >= 0){
+            if(nums1[i] >= nums2[j]){
+                nums1[idx--] = nums1[i--];
+            } else {
+                nums1[idx--] = nums2[j--];
+            }
+        }
+        while(j >= 0){
+            nums1[idx--] = nums2[j--];
+        }
+    }
+    
+    //Printsorted Array
     
 
     public static void main(String[] args) {
+        int nums1[] = {1,2,3,0,0,0};
+        int m =3;
+        int nums2[] = {2,5,6};
+        int n = 3;
+
         
     }
 }
