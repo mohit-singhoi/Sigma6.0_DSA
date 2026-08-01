f07ce78e3e781aa59e76ff6225cf448316e483e1
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-01 23:59:02 +0530

    Leetcode 4th Problem

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part1/SingleNumber.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part1/SingleNumber.java
@@ -23,6 +23,8 @@ public class SingleNumber {
        }
 
        public static void main(String[] args) {
-        
+        int nums[] = {4,1,2,1,2};
+        int result = singleNumber(nums);
+        System.out.println("Single Number : " + result);
        }
 }
