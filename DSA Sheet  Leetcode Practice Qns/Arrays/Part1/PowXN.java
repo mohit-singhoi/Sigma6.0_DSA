5ff50c9eaeeaa73efc046d929ba9fc4127e1b8ae
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-02 22:49:04 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part1/PowXN.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part1/PowXN.java
@@ -9,6 +9,15 @@
 // Input: x = 2.10000, n = 3
 // Output: 9.26100
 
+// Example 3:
+// Input: x = 2.00000, n = -2
+// Output: 0.25000
+// Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
 public class PowXN {
+    public static void main(String[] args) {
+        
+    }
+    
     
 }
