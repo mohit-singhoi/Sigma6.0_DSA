63679366caba36d5494d89c181a67d05130e8b09
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-08 00:09:35 +0530

    Search 2D Matrix Leetcode Problem Submission : 74

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part2/Search2DMatrix.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part2/Search2DMatrix.java
@@ -14,7 +14,7 @@
 
 public class Search2DMatrix {
 
-    public boolean searchInRow(int[][] matrix, int target, int row){ // O(logn)
+    public static boolean searchInRow(int[][] matrix, int target, int row){ // O(logn)
         int n =matrix[0].length;
         int st = 0, end = n-1;
         while(st <= end){
@@ -29,7 +29,7 @@ public class Search2DMatrix {
         }
         return false;
     }
-    public boolean searchMatrix(int[][] matrix, int target) { // O(logm + logn)
+    public static boolean searchMatrix(int[][] matrix, int target) { // O(logm + logn)
         int m = matrix.length, n =matrix[0].length;
 
         int startRow = 0, endRow = m-1;
@@ -49,8 +49,19 @@ public class Search2DMatrix {
         return false;
     }
 
-    public static void main(String[] args) {
-        
+  public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+
+        int target = 16;
+
+        boolean result = searchMatrix(matrix, target);
+
+        System.out.println("Result: " + result);
     }
 
 
