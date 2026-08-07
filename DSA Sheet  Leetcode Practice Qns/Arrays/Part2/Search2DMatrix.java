a636bb62cd26f73d4d9a1717f01afb1943f34262
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-08 00:06:48 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part2/Search2DMatrix.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part2/Search2DMatrix.java
@@ -14,12 +14,27 @@
 
 public class Search2DMatrix {
 
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.size(), n =matrix[0].size();
+    public boolean searchInRow(int[][] matrix, int target, int row){ // O(logn)
+        int n =matrix[0].length;
+        int st = 0, end = n-1;
+        while(st <= end){
+            int mid = st +(end-st)/2;
+            if(target == matrix[row][mid]){
+                return true;
+            } else if(target > matrix[row][mid]){
+                st = mid+1;
+            } else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) { // O(logm + logn)
+        int m = matrix.length, n =matrix[0].length;
 
         int startRow = 0, endRow = m-1;
         while(startRow <=endRow){
-            int midRow = StsrtRow +(endRow - startRow)/2;
+            int midRow = startRow +(endRow - startRow)/2;
 
             if(target >= matrix[midRow][0] && target <= matrix[midRow][n-1]){
                 return searchInRow(matrix, target, midRow);
@@ -33,5 +48,10 @@ public class Search2DMatrix {
         }
         return false;
     }
-    
+
+    public static void main(String[] args) {
+        
+    }
+
+
 }
