f035894c92f83e1ba4bf7cc9afd5e30991d0063d
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-21 23:59:54 +0530

    Leetcode 31 Problem Submission

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part3/NextPermutation.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part3/NextPermutation.java
@@ -23,10 +23,10 @@
 // Input: nums = [1,1,5]
 // Output: [1,5,1]
 
-
+import java.util.*;
 public class NextPermutation {
     
-    public void nextPermutation(int[] nums) {
+    public static  void nextPermutation(int[] nums) {
         int n = nums.length;
         int i = n - 2;
 
@@ -49,13 +49,13 @@ public class NextPermutation {
         reverse(nums, i + 1, n - 1);
     }
 
-    private void swap(int[] nums, int i, int j) {
+    private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    private void reverse(int[] nums, int start, int end) {
+    private static void reverse(int[] nums, int start, int end) {
         while (start < end) {
             swap(nums, start, end);
             start++;
@@ -63,8 +63,10 @@ public class NextPermutation {
         }
     }
 
-    
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        nextPermutation(nums);
+        System.out.println("Next permutation: " + Arrays.toString(nums));
+    }
 
-
-    
 }
