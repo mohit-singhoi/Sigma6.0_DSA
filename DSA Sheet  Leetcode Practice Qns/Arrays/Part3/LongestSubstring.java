75efdbe89b859cf57fe3ffba93cec254768a3006
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-22 17:58:49 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part3/LongestSubstring.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part3/LongestSubstring.java
@@ -15,6 +15,33 @@
 // Output: 1
 // Explanation: The answer is "b", with the length of 1.
 
+import java.util.*;
 public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        int left = 0;
+        Set<Character> set = new HashSet<>();
+
+        for (int right = 0; right < n; right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcabcbb";
+        System.out.println("Length of Longest Substring Without Repeating Characters in \"" + s1 + "\": " + lengthOfLongestSubstring(s1));
+
+        String s2 = "bbbbb";
+        System.out.println("Length of Longest Substring Without Repeating Characters in \"" + s2 + "\": " + lengthOfLongestSubstring(s2));
+    }
+
     
 }
