6a14c399272b224b8f9a162cc1e74ef7676c87dd
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-22 17:52:08 +0530

    initial commit

---
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part3/LongestSubstring.java
@@ -0,0 +1,20 @@
+// Longest Substring Without Repeating Characters : Leetcode 3
+
+// Given a string s, find the length of the longest substring without duplicate characters.
+// The Traveler's Unique Path: Avoiding Repetition A traveler explores a city, aiming to visit as many unique locations as possible without repetition.
+// If they revisit a place, they backtrack and choose a new path. This mirrors the longest substring without repeating characters problem, where each location represents a unique character. 
+// By using the sliding window technique, we efficiently track the longest unique path.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+public class LongestSubstring {
+    
+}
