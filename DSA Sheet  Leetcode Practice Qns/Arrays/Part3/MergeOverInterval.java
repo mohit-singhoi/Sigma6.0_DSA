24fb661c481968957e221d67ad248d6cbec0f310
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-22 17:34:26 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part3/MergeOverInterval.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part3/MergeOverInterval.java
@@ -10,6 +10,41 @@
 // Input: intervals = [[1,4],[4,5]]
 // Output: [[1,5]]
 
+import java.util.*;
+
 public class MergeOverInterval {
-    
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][];
+        }
+
+        // Sort the intervals based on the start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (currentInterval[1] >= intervals[i][0]) {
+                // Overlapping intervals, merge them
+                currentInterval[1] = Math.max(currentInterval[1], intervals[i][1]);
+            } else {
+                // No overlap, add the current interval to the merged list
+                merged.add(currentInterval);
+                currentInterval = intervals[i];
+            }
+        }
+
+        // Add the last interval
+        merged.add(currentInterval);
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    // Main method to test the merge function
+    public static void main(String[] args) {
+        int[][] intervals1 = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
+        int[][] mergedIntervals1 = merge(intervals1);
+        System.out.println("Merged Intervals 1: " + Arrays.deepToString(mergedIntervals1));
+    }
 }
