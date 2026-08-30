1c60e3de43a6f703d03247e689dc917bf2e79b73
Mohit kumar <mohitsinghoi501@gmail.com>
2026-08-30 22:00:09 +0530

    initial commit

---
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part3/MatrixZero.java
@@ -0,0 +1,20 @@
+// Set Matrix Zeroes problem: Leetcode 73
+
+// Given an m x n integer matrix, if an element is 0, set its entire row and column to 0's. You must do it in place.
+// 0-1 Matrix Problem : Hotel Maintenance
+// Imagine a hotel where rooms are arranged in a grid, like a 0-1 matrix. If a room has a major issue (marked as 0), the entire row and column must be closed for repairs.
+// Instead of manually tracking, a smart system updates all affected rooms automatically, just like how we modify a 0-1 matrix to set entire rows and columns to zero when a 0 is found. 
+// This helps manage problems efficiently without affecting other rooms.
+
+// Example 1:
+// Input: matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+// Example 2:
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+
+public class MatrixZero {
+    
+}
