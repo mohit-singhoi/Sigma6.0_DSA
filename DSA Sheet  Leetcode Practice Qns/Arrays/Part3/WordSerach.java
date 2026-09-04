f6edcbcbfdc3ecaa96c2c5d1475e78e465b4bca6
Mohit kumar <mohitsinghoi501@gmail.com>
2026-09-04 21:40:08 +0530

    initial commit

--- DSA Sheet  Leetcode Practice Qns/Arrays/Part3/WordSerach.java
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part3/WordSerach.java
@@ -10,12 +10,53 @@
 // Output : true
 
 public class WordSerach {
+        private int[] r = { -1, 1, 0, 0 }; // Row movement directions
+        private int[] c = { 0, 0, -1, 1 }; // Column movement directions
+
+        private boolean helper(int row, int col, char[][] board, int n, int m, int index, String word, int size) {
+            if (index == size)
+                return true;
+
+            char ch = board[row][col];
+            board[row][col] = '$'; // Mark as visited
+
+            for (int i = 0; i < 4; i++) {
+                int ur = row + r[i];
+                int uc = col + c[i];
+
+                if (ur >= 0 && ur < n && uc >= 0 && uc < m) {
+                    if (board[ur][uc] == word.charAt(index)) {
+                        if (helper(ur, uc, board, n, m, index + 1, word, size))
+                            return true;
+                    }
+                }
+            }
+
+            board[row][col] = ch; // Restore the character
+            return false;
+        }
+
+        public static  boolean exist(char[][] board, String word) {
+            int n = board.length;
+            int m = board[0].length;
+            int size = word.length();
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (board[i][j] == word.charAt(0)) {
+                        if (helper(i, j, board, n, m, 1, word, size))
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
 
     public static void main(String[] args) {
         char[][] board = {
-                {'A', 'B', 'C', 'E'},
-                {'S', 'F', 'C', 'S'},
-                {'A', 'D', 'E', 'E'}
+                { 'A', 'B', 'C', 'E' },
+                { 'S', 'F', 'C', 'S' },
+                { 'A', 'D', 'E', 'E' }
         };
         String word = "ABCCED";
         System.out.println(exist(board, word)); // Output: true
@@ -26,5 +67,5 @@ public class WordSerach {
         String word3 = "ABCB";
         System.out.println(exist(board, word3)); // Output: false
     }
-    
+
 }
