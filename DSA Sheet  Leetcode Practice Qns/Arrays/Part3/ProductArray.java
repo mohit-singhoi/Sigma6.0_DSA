aa58aab37b82242fbbdb9eaba87d89969e7ffd18
Mohit kumar <mohitsinghoi501@gmail.com>
2026-09-04 21:51:42 +0530

    initial commit

---
+++ DSA Sheet  Leetcode Practice Qns/Arrays/Part3/ProductArray.java
@@ -0,0 +1,11 @@
+// Product of Array Except Self : Leetcode 238
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. 
+
+// Example 1 :
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+public class ProductArray {
+    
+}
